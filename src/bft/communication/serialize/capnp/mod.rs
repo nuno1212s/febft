c0b53a366856bf44e213c5192847f16ea9a6b404
Tiago Carvalho <up201605259@fc.up.pt>
2020-11-18 20:01:23 +0000

    finish serialize module

--- src/bft/communication/serialize/capnp/mod.rs
+++ src/bft/communication/serialize/capnp/mod.rs
@@ -1,25 +1,36 @@
 use std::io;
+use std::default::Default;
 
 use capnp::message::HeapAllocator;
 
 use crate::bft::communication::socket::Socket;
 use crate::bft::communication::message::{ReplicaMessage, ClientMessage};
 
-pub struct Serializer {
-    allocator: HeapAllocator,
+pub async fn serialize_to_replica(s: &mut Socket, m: ReplicaMessage) -> io::Result<()> {
+    let mut root = capnp::message::Builder::new(HeapAllocator::new());
+    let mut message_builder: message::replica_message::Builder = root.init_root();
+    match m {
+        ReplicaMessage::Dummy(data) => message_builder.set_dummy(&data),
+    };
+    capnp_futures::serialize::write_message(s, root)
+        .await
+        .map_err(|e| io::Error::new(io::ErrorKind::Other, format!("Failed to serialize using capnp: {}", e)))
 }
 
-impl Serializer {
-    pub fn new() -> Self {
-        Serializer { allocator: HeapAllocator::new() }
-    }
+pub async fn deserialize_from_replica(s: &mut Socket) -> io::Result<ReplicaMessage> {
+    let root = capnp_futures::serialize::read_message(s, Default::default())
+        .await
+        .map_err(|e| io::Error::new(io::ErrorKind::Other, format!("Failed to deserialize using capnp: {}", e)))?;
 
-    pub async fn serialize_to_replica(&mut self, s: &mut Socket, m: ReplicaMessage) -> io::Result<()> {
-        unimplemented!();
-    }
-
-    pub async fn deserialize_from_replica(&mut self, s: &mut Socket) -> io::Result<ReplicaMessage> {
-        unimplemented!();
+    match root {
+        Some(root) => {
+            let message_reader: message::replica_message::Reader = root.get_root()
+                .map_err(|e| io::Error::new(io::ErrorKind::Other, format!("Failed to get root: {}", e)))?;
+            let dummy = message_reader.get_dummy()
+                .map_err(|e| io::Error::new(io::ErrorKind::Other, format!("Failed to get dummy: {}", e)))?;
+            Ok(ReplicaMessage::Dummy(dummy.into()))
+        },
+        None => Err(io::Error::new(io::ErrorKind::Other, "No message read from socket.")),
     }
 }
 
--- src/bft/communication/serialize/mod.rs
+++ src/bft/communication/serialize/mod.rs
@@ -8,27 +8,16 @@ use futures::io::AsyncWriteExt;
 use crate::bft::communication::socket::Socket;
 use crate::bft::communication::message::{ReplicaMessage, ClientMessage};
 
-pub struct Serializer {
-    #[cfg(feature = "serialize_capnp")]
-    inner: capnp::Serializer,
+pub async fn serialize_to_replica(s: &mut Socket, m: ReplicaMessage) -> io::Result<()> {
+    let () = {
+        #[cfg(feature = "serialize_capnp")]
+        capnp::serialize_to_replica(s, m).await?
+    };
+    s.flush().await?;
+    Ok(())
 }
 
-impl Serializer {
-    pub fn new() -> Self {
-        let inner = {
-            #[cfg(feature = "serialize_capnp")]
-            capnp::Serializer::new()
-        };
-        Serializer { inner }
-    }
-
-    pub async fn serialize_to_replica(&mut self, s: &mut Socket, m: ReplicaMessage) -> io::Result<()> {
-        self.inner.serialize_to_replica(s, m).await?;
-        s.flush().await?;
-        Ok(())
-    }
-
-    pub async fn deserialize_from_replica(&mut self, s: &mut Socket) -> io::Result<ReplicaMessage> {
-        self.inner.deserialize_from_replica(s).await
-    }
+pub async fn deserialize_from_replica(s: &mut Socket) -> io::Result<ReplicaMessage> {
+    #[cfg(feature = "serialize_capnp")]
+    capnp::deserialize_from_replica(s).await
 }
