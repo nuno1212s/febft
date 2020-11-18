752ffd01058edb6906a93c3caa272aa0beb8db5d
Tiago Carvalho <up201605259@fc.up.pt>
2020-11-18 16:57:09 +0000

    change fn name

--- src/bft/communication/serialize/mod.rs
+++ src/bft/communication/serialize/mod.rs
@@ -8,7 +8,7 @@ use futures::io::AsyncWriteExt;
 use crate::bft::communication::socket::Socket;
 use crate::bft::communication::message::{ReplicaMessage, ClientMessage};
 
-pub async fn to_replica(s: &mut Socket, m: ReplicaMessage) -> io::Result<()> {
+pub async fn serialize_to_replica(s: &mut Socket, m: ReplicaMessage) -> io::Result<()> {
     #[cfg(feature = "serialize_capnp")]
     {
         capnp::serialize_to_replica(s, m).await?;
