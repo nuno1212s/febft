dea513b97ca50bb746995c4a870cc2aa26a0da48
Tiago Carvalho <up201605259@fc.up.pt>
2020-11-18 16:05:04 +0000

    serialize to replica

--- src/bft/communication/serialize/mod.rs
+++ src/bft/communication/serialize/mod.rs
@@ -1,7 +1,18 @@
 #[cfg(feature = "serialize_capnp")]
 mod capnp;
 
-#[cfg(feature = "serialize_capnp")]
-pub fn serialize() {
-    unimplemented!();
+use std::io;
+
+use futures::io::AsyncWriteExt;
+
+use crate::bft::communication::socket::Socket;
+use crate::bft::communication::message::{ReplicaMessage, ClientMessage};
+
+pub async fn to_replica(s: &mut Socket, m: ReplicaMessage) -> io::Result<()> {
+    #[cfg(feature = "serialize_capnp")]
+    {
+        capnp::serialize_to_replica(s, m).await?;
+    }
+    w.flush().await?;
+    Ok(())
 }
