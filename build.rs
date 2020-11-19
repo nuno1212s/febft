5108e0e2ca7b5b5082714ca0918a5dbf432e2419
Tiago Carvalho <up201605259@fc.up.pt>
2020-11-19 04:07:10 +0000

    generate error kinds on the fly

--- build.rs
+++ build.rs
@@ -1,6 +1,18 @@
-const CAPNP_SRC: &str = "src/bft/communication/serialize/capnp/message.capnp";
+use std::fs;
+use std::path::PathBuf;
+use std::io::{Write, BufWriter};
+
+use itertools::Itertools;
+
+const MESSAGE_CAPNP_SRC: &str = "src/bft/communication/serialize/capnp/message.capnp";
+const ERROR_KIND_DST: &str = "error_kind.rs";
 
 fn main() {
+    generate_error_kinds();
+    generate_message_from_capnp();
+}
+
+fn generate_message_from_capnp() {
     // check if we need to compile capnp
     match std::env::var("CARGO_FEATURE_SERIALIZE_CAPNP") {
         Ok(_) => (),
@@ -8,9 +20,70 @@ fn main() {
     };
 
     // recompile capnp message into rust when the source changes
-    println!("cargo:rerun-if-changed={}", CAPNP_SRC);
+    println!("cargo:rerun-if-changed={}", MESSAGE_CAPNP_SRC);
     capnpc::CompilerCommand::new()
-        .file(CAPNP_SRC)
+        .file(MESSAGE_CAPNP_SRC)
         .run()
         .unwrap();
+}
+
+fn generate_error_kinds() {
+    fn generate(mut pbuf: &mut PathBuf, mut name_buf: &mut Vec<String>, mut buf: &mut BufWriter<fs::File>) {
+        let dir_ents = fs::read_dir(&mut pbuf).unwrap();
+
+        for ent in dir_ents {
+            let dir_name = ent
+                .iter()
+                .flat_map(|ent| ent.file_type().map(|t| (t, ent.file_name())))
+                .map(|(t, name)| if t.is_dir() { Some(name) } else { None })
+                .nth(0)
+                .unwrap_or(None);
+            let orig_dir_name = match dir_name {
+                Some(n) => n,
+                _ => continue,
+            };
+            let dir_name = orig_dir_name
+                .as_os_str()
+                .to_str()
+                .unwrap()
+                .split('_')
+                .flat_map(|part| {
+                    let (first, rest) = part.split_at(1);
+                    first
+                        .chars()
+                        .map(|ch| ch.to_ascii_uppercase())
+                        .chain(rest.chars())
+                })
+                .join("");
+            name_buf.push(dir_name);
+            writeln!(buf, "    {},", name_buf.join("")).unwrap();
+            pbuf.push(orig_dir_name);
+            generate(&mut pbuf, &mut name_buf, &mut buf);
+        }
+
+        pbuf.pop();
+        name_buf.pop();
+    }
+
+    let path_buf = {
+        let mut b = PathBuf::from(std::env::var("OUT_DIR").unwrap());
+        b.push(ERROR_KIND_DST);
+        b
+    };
+    let file = fs::File::create(path_buf).unwrap();
+    let mut buf = BufWriter::new(file);
+
+    let mut path_buf = {
+        let mut b = PathBuf::from(std::env::var("CARGO_MANIFEST_DIR").unwrap());
+        b.push("src");
+        b.push("bft");
+        b
+    };
+    let mut name_buf = Vec::new();
+
+    writeln!(&mut buf, r#"/// Includes a list of all the library software components.
+#[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+pub enum ErrorKind {{"#).unwrap();
+    generate(&mut path_buf, &mut name_buf, &mut buf);
+    writeln!(&mut buf, "}}").unwrap();
 }
--- src/bft/error/mod.rs
+++ src/bft/error/mod.rs
@@ -1,17 +1,10 @@
 use std::fmt;
 use std::error;
+use std::result;
 
 /// Wrapper Result for the Rust standard library Result type.
 // TODO: create an actual error type to use instead of ()
-pub type Result<T> = std::result::Result<T, ()>;
-
-/// Includes a list of all the library software components.
-#[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
-pub enum ErrorKind {
-    AsyncRuntime,
-    Communication,
-    Other,
-}
+pub type Result<T> = result::Result<T, ()>;
 
 /// The error type used throughout this crate.
 pub struct Error {
@@ -28,4 +21,10 @@ impl fmt::Debug for Error {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         fmt::Debug::fmt(&self.inner, f)
     }
+}
+
+pub use error_kind::ErrorKind;
+
+mod error_kind {
+    include!(concat!(env!("OUT_DIR"), "/error_kind.rs"));
 }
