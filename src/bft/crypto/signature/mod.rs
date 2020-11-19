e9124dfd23f2badc5b6ba49f88b89e4ebd0351ed
Tiago Carvalho <up201605259@fc.up.pt>
2020-11-19 21:44:41 +0000

    start writing signatures abstraction

---
+++ src/bft/crypto/signature/mod.rs
@@ -0,0 +1,6 @@
+#[cfg(feature = "crypto_signature_ring_ed25519")]
+mod ring;
+
+pub struct SecretKey {
+    // dummy
+}
--- src/bft/mod.rs
+++ src/bft/mod.rs
@@ -1,4 +1,5 @@
 pub mod async_runtime;
 pub mod communication;
 pub mod context;
+pub mod crypto;
 pub mod error;
