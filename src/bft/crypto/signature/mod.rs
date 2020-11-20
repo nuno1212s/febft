2d3f7c698439a17f1a69c6bef7f659470e29d1c3
Tiago Carvalho <up201605259@fc.up.pt>
2020-11-20 20:03:23 +0000

    finish ring signatures

--- src/bft/crypto/signature/mod.rs
+++ src/bft/crypto/signature/mod.rs
@@ -1,12 +1,40 @@
+use crate::bft::error::prelude::*;
+
 #[cfg(feature = "crypto_signature_ring_ed25519")]
 mod ring_ed25519;
 
-pub struct PublicKey {
+pub struct KeyPair {
     #[cfg(feature = "crypto_signature_ring_ed25519")]
-    inner: ring_ed25519::PublicKey,
+    inner: ring_ed25519::KeyPair,
 }
 
-pub struct SecretKey {
+#[derive(Copy, Clone)]
+pub struct Signature {
     #[cfg(feature = "crypto_signature_ring_ed25519")]
-    inner: ring_ed25519::SecretKey,
+    inner: ring_ed25519::Signature,
+}
+
+impl KeyPair {
+    fn from_bytes(raw_bytes: &[u8]) -> Result<Self> {
+        let inner = {
+            #[cfg(feature = "crypto_signature_ring_ed25519")]
+            ring_ed25519::KeyPair::from_bytes(raw_bytes)?
+        };
+        Ok(KeyPair { inner })
+    }
+
+    fn sign(&self, message: &[u8]) -> Result<Signature> {
+        let inner = self.inner.sign(message)?;
+        Ok(Signature { inner })
+    }
+
+    fn verify(&self, message: &[u8], signature: &Signature) -> Result<()> {
+        self.inner.verify(message, &signature.inner)
+    }
+}
+
+impl AsRef<[u8]> for Signature {
+    fn as_ref(&self) -> &[u8] {
+        self.inner.as_ref()
+    }
 }
--- src/bft/crypto/signature/ring_ed25519/mod.rs
+++ src/bft/crypto/signature/ring_ed25519/mod.rs
@@ -1,3 +1,29 @@
-pub struct PublicKey;
+use ring::{signature as rsig, signature::KeyPair as RKeyPair};
 
-pub struct SecretKey;
+use crate::bft::error::prelude::*;
+
+pub struct KeyPair {
+    sk: rsig::Ed25519KeyPair,
+    pk: rsig::UnparsedPublicKey<<rsig::Ed25519KeyPair as RKeyPair>::PublicKey>,
+}
+
+pub type Signature = ring::signature::Signature;
+
+impl KeyPair {
+    pub fn from_bytes(seed_bytes: &[u8]) -> Result<Self> {
+        let sk = rsig::Ed25519KeyPair::from_seed_unchecked(seed_bytes)
+            .simple_msg(ErrorKind::CryptoSignatureRingEd25519, "Invalid seed for ed25519 key")?;
+        let pk = sk.public_key().clone();
+        let pk = rsig::UnparsedPublicKey::new(&rsig::ED25519, pk);
+        Ok(KeyPair { pk, sk })
+    }
+
+    pub fn sign(&self, message: &[u8]) -> Result<Signature> {
+        Ok(self.sk.sign(message))
+    }
+
+    pub fn verify(&self, message: &[u8], signature: &Signature) -> Result<()> {
+        self.pk.verify(message, signature.as_ref())
+            .simple_msg(ErrorKind::CryptoSignatureRingEd25519, "Invalid signature")
+    }
+}
