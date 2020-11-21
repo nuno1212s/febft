0bc7bd35f64ff7fdb03a23c1fa98354f26fd415c
Tiago Carvalho <up201605259@fc.up.pt>
2020-11-21 03:22:22 +0000

    move bench elsewhere

---
+++ src/bench/mod.rs
@@ -0,0 +1,45 @@
+//use futures::
+use crate::bft::async_runtime as runtime;
+use crate::bft::threadpool;
+//use crate::bft::socket
+
+pub enum Side {
+    Client,
+    Server,
+}
+
+pub fn layered_bench(side: Side) {
+    let async_threads: usize = std::env::var("ASYNC_THREADS")
+        .unwrap()
+        .parse()
+        .unwrap();
+
+    // start async runtime
+    runtime::init(async_threads).unwrap();
+
+    match side {
+        Side::Client => runtime::block_on(layered_bench_client()),
+        Side::Server => runtime::block_on(layered_bench_server()),
+    }
+}
+
+async fn layered_bench_client() {
+    unimplemented!();
+}
+
+async fn layered_bench_server() {
+    let pool_threads: usize = std::env::var("POOL_THREADS")
+        .unwrap()
+        .parse()
+        .unwrap();
+
+    let pool = threadpool::Builder::new()
+        .num_threads(pool_threads)
+        .build();
+
+    runtime::spawn(async move {
+        unimplemented!();
+    });
+
+    // finish this
+}
--- src/lib.rs
+++ src/lib.rs
@@ -4,13 +4,8 @@ pub mod bft;
 #[cfg(not(feature = "expose_impl"))]
 mod bft;
 
+#[cfg(feature = "bench")]
+pub mod bench;
+
 // let library users have access to our result type
 pub use bft::error::Result;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
