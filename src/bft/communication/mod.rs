feebbbf1d6a406ea8fb33f4ec6d7c4f64b82f07d
Tiago Carvalho <up201605259@fc.up.pt>
2020-11-21 03:06:36 +0000

    begin writing bench

--- src/bft/communication/mod.rs
+++ src/bft/communication/mod.rs
@@ -1,4 +1,9 @@
+#[cfg(not(feature = "expose_impl"))]
 mod socket;
+
+#[cfg(feature = "expose_impl")]
+pub mod socket;
+
 pub mod serialize;
 pub mod message;
 
@@ -24,9 +29,47 @@ pub struct NodeId(u32);
 //    pub async fn send(&self, ctx: &Context, 
 //}
 
-#[cfg(feature = "expose_impl")]
+#[cfg(feature = "bench")]
 pub mod bench {
-    pub fn layered_bench() {
-        unimplemented!();
+    //use futures::
+    use crate::bft::async_runtime as runtime;
+    use crate::bft::threadpool;
+    //use crate::bft::socket
+
+    pub enum Side {
+        Client,
+        Server,
+    }
+
+    pub fn layered_bench(side: Side) {
+        let async_threads: usize = std::env::var("ASYNC_THREADS")
+            .unwrap()
+            .parse()
+            .unwrap();
+
+        // start async runtime
+        runtime::init(async_threads).unwrap();
+
+        match side {
+            Side::Client => runtime::block_on(layered_bench_client()),
+            Side::Server => runtime::block_on(layered_bench_server()),
+        }
+    }
+
+    async fn layered_bench_server() {
+        let pool_threads: usize = std::env::var("POOL_THREADS")
+            .unwrap()
+            .parse()
+            .unwrap();
+
+        let pool = threadpool::Builder::new()
+            .num_threads(pool_threads)
+            .build();
+
+        runtime::spawn(async move {
+            asd
+        });
+
+        // finish this
     }
 }
