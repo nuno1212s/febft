a3379cf7b808caaa6dda2cc30db5657b1f9a3d66
Tiago Carvalho <up201605259@fc.up.pt>
2020-11-26 18:52:10 +0000

    serialize revamp

--- src/bft/communication/serialize/capnp/mod.rs
+++ src/bft/communication/serialize/capnp/mod.rs
@@ -1,59 +1,26 @@
 use std::default::Default;
 
+use capnp::serialize;
 use capnp::message::HeapAllocator;
-use futures::io::{AsyncRead, AsyncWrite, AsyncWriteExt};
+use bytes::{Buf, BufMut};
 
 use crate::bft::error::*;
 use crate::bft::communication::message::{ReplicaMessage, ClientMessage};
 
-pub struct Serializer<W> {
-    inner: W,
-}
-
-pub struct Deserializer<R> {
-    inner: R,
-}
-
-impl<W: Unpin + AsyncWrite> Serializer<W> {
-    pub fn new(inner: W) -> Self {
-        Self { inner }
-    }
-
-    pub async fn to_replica(&mut self, m: ReplicaMessage) -> Result<()> {
-        serialize_to_replica(&mut self.inner, m).await?;
-        self.inner.flush()
-            .await
-            .wrapped_msg(ErrorKind::CommunicationSerialize, "Failed to flush socket")?;
-        Ok(())
-    }
-}
-
-impl<R: Unpin + AsyncRead> Deserializer<R> {
-    pub fn new(inner: R) -> Self {
-        Self { inner }
-    }
-
-    pub async fn from_replica(&mut self) -> Result<ReplicaMessage> {
-        deserialize_from_replica(&mut self.inner).await
-    }
-}
-
-#[inline]
-async fn serialize_to_replica<W: Unpin + AsyncWrite>(w: W, m: ReplicaMessage) -> Result<()> {
+pub fn serialize_to_replica<B: BufMut>(buf: B, m: ReplicaMessage) -> Result<B> {
     let mut root = capnp::message::Builder::new(HeapAllocator::new());
     let mut message_builder: message::replica_message::Builder = root.init_root();
     match m {
         ReplicaMessage::Dummy(data) => message_builder.set_dummy(&data),
     };
-    capnp_futures::serialize::write_message(w, root)
-        .await
+    let mut writer = buf.writer();
+    serialize::write_message(&mut writer, &root)
+        .map(|()| writer.into_inner())
         .wrapped_msg(ErrorKind::CommunicationSerializeCapnp, "Failed to serialize using capnp")
 }
 
-#[inline]
-async fn deserialize_from_replica<R: Unpin + AsyncRead>(r: R) -> Result<ReplicaMessage> {
-    let root = capnp_futures::serialize::read_message(r, Default::default())
-        .await
+pub fn deserialize_from_replica<B: Buf>(buf: B) -> Result<ReplicaMessage> {
+    let root = capnp_futures::serialize::read_message(buf.reader(), Default::default())
         .wrapped_msg(ErrorKind::CommunicationSerializeCapnp, "Failed to deserialize using capnp")?;
 
     match root {
--- src/bft/communication/serialize/serde/bincode/mod.rs
+++ src/bft/communication/serialize/serde/bincode/mod.rs
@@ -1,105 +1,18 @@
-use std::marker::PhantomData;
-
-use serde::{Serialize, Deserialize};
-use futures::io::{AsyncRead, AsyncWrite};
-use futures_codec::{Encoder, Decoder, BytesMut};
 use bytes::{Buf, BufMut};
+use serde::{Serialize, Deserialize};
 
 use crate::bft::error::*;
+use crate::bft::communication::message::{ReplicaMessage, ClientMessage};
 
-pub type Serializer<W> = super::Serializer<W, BincodeCodec<super::Message, super::Message>>;
-
-pub type Deserializer<R> = super::Deserializer<R, BincodeCodec<super::Message, super::Message>>;
-
-pub struct BincodeCodec<Enc, Dec> {
-    _phantom: PhantomData<(Enc, Dec)>,
+pub fn serialize_to_replica<B: BufMut>(buf: B, m: ReplicaMessage) -> Result<B> {
+    let mut w = buf.writer();
+    bincode::serialize_into(&mut w, &m)
+        .map(|()| w.into_inner())
+        .wrapped(ErrorKind::CommunicationSerializeSerdeBincode)
 }
 
-pub fn new_serializer<W: Unpin + AsyncWrite>(writer: W) -> Serializer<W> {
-    Serializer::new(writer, BincodeCodec::new())
-}
-
-pub fn new_deserializer<R: Unpin + AsyncRead>(reader: R) -> Deserializer<R> {
-    Deserializer::new(reader, BincodeCodec::new())
-}
-
-impl<Enc, Dec> Decoder for BincodeCodec<Enc, Dec>
-where
-    for<'de> Dec: Deserialize<'de> + 'static,
-    for<'de> Enc: Serialize + 'static,
-{
-    type Item = Dec;
-    type Error = Error;
-
-    fn decode(&mut self, buf: &mut BytesMut) -> Result<Option<Self::Item>> {
-        // Build cursor
-        let mut c = std::io::Cursor::new(&buf);
-
-        // Attempt deserialization
-        let res: std::result::Result<Dec, _> = bincode::deserialize_from(&mut c);
-
-        // If we ran out before parsing, return none and try again later
-        let res = match res {
-            Ok(v) => Ok(Some(v)),
-            Err(e) => {
-                match *e {
-                    bincode::ErrorKind::Io(e) if e.kind() == std::io::ErrorKind::UnexpectedEof => {
-                        Ok(None)
-                    },
-                    _ => {
-                        Err(Error::wrapped(ErrorKind::CommunicationSerializeSerdeBincode, e))
-                    },
-                }
-            },
-        };
-
-        // Update offset from iterator
-        let offset = c.position() as usize;
-
-        // Advance buffer
-        buf.advance(offset);
-
-        res
-    }
-}
-
-impl<Enc, Dec> Encoder for BincodeCodec<Enc, Dec>
-where
-    for<'de> Dec: Deserialize<'de> + 'static,
-    for<'de> Enc: Serialize + 'static,
-{
-    type Item = Enc;
-    type Error = Error;
-
-    fn encode(&mut self, data: Self::Item, buf: &mut BytesMut) -> Result<()> {
-        // Encode bincode
-        let j = bincode::serialize(&data)
-            .wrapped(ErrorKind::CommunicationSerializeSerdeBincode)?;
-
-        // Write to buffer
-        buf.reserve(j.len());
-        buf.put_slice(&j);
-
-        Ok(())
-    }
-}
-
-impl<Enc, Dec> Clone for BincodeCodec<Enc, Dec>
-where
-    for<'de> Dec: Deserialize<'de> + 'static,
-    for<'de> Enc: Serialize + 'static,
-{
-    fn clone(&self) -> Self {
-        BincodeCodec::new()
-    }
-}
-
-impl<Enc, Dec> BincodeCodec<Enc, Dec>
-where
-    for<'de> Dec: Deserialize<'de> + 'static,
-    for<'de> Enc: Serialize + 'static,
-{
-    pub fn new() -> Self {
-        Self { _phantom: PhantomData }
-    }
+pub fn deserialize_from_replica<B: Buf>(buf: B) -> Result<ReplicaMessage> {
+    let mut r = buf.reader();
+    bincode::deserialize_from(&mut r)
+        .wrapped(ErrorKind::CommunicationSerializeSerdeBincode)
 }
