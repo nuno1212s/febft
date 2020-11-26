d472a4350dfe75d6c1a62cfeff2dc36c85206e16
Tiago Carvalho <up201605259@fc.up.pt>
2020-11-26 19:46:29 +0000

    fix bench

--- src/bench/mod.rs
+++ src/bench/mod.rs
@@ -7,13 +7,15 @@ use futures_timer::Delay;
 use futures::channel::mpsc;
 use futures::sink::SinkExt;
 use futures::stream::StreamExt;
+use futures::io::{AsyncReadExt, AsyncWriteExt};
 use async_semaphore::Semaphore;
+use bytes::{Buf, BufMut};
 
 use crate::bft::async_runtime as runtime;
 use crate::bft::threadpool;
 use crate::bft::communication::serialize::{
-    Serializer,
-    Deserializer,
+    serialize_to_replica,
+    deserialize_from_replica,
 };
 use crate::bft::communication::socket::{
     self,
@@ -24,6 +26,7 @@ use crate::bft::crypto::signature::{Signature, KeyPair};
 
 pub struct Shared {
     io: AtomicU64,
+    des: AtomicU64,
     sigs: AtomicU64,
     keypair: KeyPair,
     pool: threadpool::ThreadPool,
@@ -73,6 +76,7 @@ async fn layered_bench_client(async_threads: usize, message_len: usize, addr: &s
 
     // message producer -- produces as fast as the
     // bound in the sem channel
+    let rt_pool = pool.clone();
     runtime::spawn(async move {
         let keypair = Arc::new({
             let buf = [0; 32];
@@ -82,7 +86,7 @@ async fn layered_bench_client(async_threads: usize, message_len: usize, addr: &s
             let sem_guard = Semaphore::acquire_arc(&sem).await;
             let keypair = Arc::clone(&keypair);
             let (rsp_tx, rsp_rx) = oneshot::channel();
-            pool.execute(move || {
+            rt_pool.clone().execute(move || {
                 let _sem_guard = sem_guard;
                 let mut msg = vec![0; message_len];
                 let sig = keypair.sign(&msg).unwrap();
@@ -94,33 +98,22 @@ async fn layered_bench_client(async_threads: usize, message_len: usize, addr: &s
         }
     });
 
-    #[cfg(feature = "serialize_capnp")]
-    let (mut spawned_tasks, set) = {
-        (0_usize, runtime::LocalSet::new())
-    };
-
     // client spawner
-    while let Ok(s) = socket::connect(addr).await {
+    while let Ok(mut s) = socket::connect(addr).await {
         let dummy = new_message.next().await.unwrap();
-
-        #[cfg(not(feature = "serialize_capnp"))]
+        let pool = pool.clone();
         runtime::spawn(async move {
-            let mut serializer = Serializer::new(s);
-            serializer.to_replica(dummy).await.unwrap();
-        });
-
-        #[cfg(feature = "serialize_capnp")]
-        {
-            set.spawn_local(async move {
-                let mut serializer = Serializer::new(s);
-                serializer.to_replica(dummy).await.unwrap();
+            let (rsp_tx, rsp_rx) = oneshot::channel();
+            pool.execute(move || {
+                let mut size = [0; 8];
+                let serialized = serialize_to_replica(Vec::new(), dummy).unwrap();
+                (&mut size[..]).put_u64(serialized.len() as u64);
+                rsp_tx.send((size, serialized)).unwrap();
             });
-            spawned_tasks = spawned_tasks.wrapping_add(1);
-            if spawned_tasks % async_threads == 0 {
-                let delay = Delay::new(Duration::from_nanos(20));
-                set.run_until(delay).await;
-            }
-        }
+            let (size, serialized) = rsp_rx.await.unwrap();
+            s.write_all(&size[..]).await.unwrap();
+            s.write_all(&serialized).await.unwrap();
+        });
     }
 }
 
@@ -147,6 +140,7 @@ async fn layered_bench_server(message_len: usize, addr: &str) {
         pool,
         keypair,
         io: AtomicU64::new(0),
+        des: AtomicU64::new(0),
         sigs: AtomicU64::new(0),
     });
 
@@ -184,20 +178,36 @@ async fn layered_bench_server(message_len: usize, addr: &str) {
 
     // show results
     let io_throughput = shared.io.load(Ordering::Relaxed);
+    let des_throughput = shared.des.load(Ordering::Relaxed);
     let sig_throughput = shared.sigs.load(Ordering::Relaxed);
 
-    println!("IO throughput        => {} ops per second", (io_throughput as f64) / 5.0);
-    println!("Signature throughput => {} ops per second", (sig_throughput as f64) / 5.0);
+    println!("IO throughput          => {} ops per second", (io_throughput as f64) / 5.0);
+    println!("Deserialize throughput => {} ops per second", (des_throughput as f64) / 5.0);
+    println!("Signature throughput   => {} ops per second", (sig_throughput as f64) / 5.0);
 }
 
-async fn handle_one_request(message_len: usize, s: Socket, shared: Arc<Shared>) {
-    let mut deserializer = Deserializer::new(s);
-    let ReplicaMessage::Dummy(dummy) = deserializer.from_replica().await.unwrap();
+async fn handle_one_request(message_len: usize, mut s: Socket, shared: Arc<Shared>) {
+    let buf = {
+        let mut buf = [0; 8];
+        s.read_exact(&mut buf[..]).await.unwrap();
+        let size = (&buf[..]).get_u64() as usize;
+
+        let mut buf = vec![0; size];
+        s.read_exact(&mut buf[..]).await.unwrap();
+
+        buf
+    };
     shared.io.fetch_add(1, Ordering::Relaxed);
+
     shared.pool.clone().execute(move || {
-        let msg = &dummy[..message_len];
-        let sig = Signature::from_bytes(&dummy[message_len..]).unwrap();
-        shared.keypair.verify(msg, &sig).unwrap();
-        shared.sigs.fetch_add(1, Ordering::Relaxed);
+        let ReplicaMessage::Dummy(dummy) = deserialize_from_replica(&*buf).unwrap();
+        shared.des.fetch_add(1, Ordering::Relaxed);
+
+        shared.pool.clone().execute(move || {
+            let msg = &dummy[..message_len];
+            let sig = Signature::from_bytes(&dummy[message_len..]).unwrap();
+            shared.keypair.verify(msg, &sig).unwrap();
+            shared.sigs.fetch_add(1, Ordering::Relaxed);
+        });
     });
 }
