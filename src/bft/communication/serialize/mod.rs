a60a37177840d0fe12b0a8aeb11ee7feb4ca3b61
Tiago Carvalho <up201605259@fc.up.pt>
2021-03-22 18:15:52 +0000

    begin to abstract over capnp and serde for serialization

--- src/bft/communication/serialize/mod.rs
+++ src/bft/communication/serialize/mod.rs
@@ -1,18 +1,33 @@
+// TODO: create a trait for capnp serialization
+// * serialize takes (SystemMessage, capnp::message::Builder) and returns ()
+// * deserialize takes (capnp::message::Builder) and returns SystemMessage
+// * no reads or writes performed by the trait
+
 #[cfg(feature = "serialize_capnp")]
 mod capnp;
 
 #[cfg(feature = "serialize_serde")]
 mod serde;
 
+#[cfg(feature = "serialize_serde")]
+use serde::{Serialize, Deserialize};
+
 use bytes::{Buf, BufMut};
 
 use crate::bft::error::*;
-use crate::bft::communication::message::ReplicaMessage;
+use crate::bft::communication::message::SystemMessage;
 
-pub fn serialize_to_replica<B: BufMut>(buf: B, m: ReplicaMessage) -> Result<B> {
-    #[cfg(feature = "serialize_capnp")]
-    { capnp::serialize_to_replica(buf, m) }
+#[cfg(feature = "serialize_capnp")]
+pub fn serialize_message<O, B: BufMut>(buf: B, m: SystemMessage<O>) -> Result<B> {
+    capnp::serialize_to_replica(buf, m)
+}
 
+#[cfg(feature = "serialize_serde")]
+pub fn serialize_message<O, B>(buf: B, m: SystemMessage<O>) -> Result<B>
+where
+    O: Serialize,
+    B: BufMut,
+{
     #[cfg(feature = "serialize_serde_bincode")]
     { serde::bincode::serialize_to_replica(buf, m) }
 
@@ -23,10 +38,17 @@ pub fn serialize_to_replica<B: BufMut>(buf: B, m: ReplicaMessage) -> Result<B> {
     { serde::cbor::serialize_to_replica(buf, m) }
 }
 
-pub fn deserialize_from_replica<B: Buf>(buf: B) -> Result<ReplicaMessage> {
-    #[cfg(feature = "serialize_capnp")]
-    { capnp::deserialize_from_replica(buf) }
+#[cfg(feature = "serialize_capnp")]
+pub fn deserialize_message<O, B: Buf>(buf: B) -> Result<SystemMessage<O>> {
+    capnp::deserialize_from_replica(buf)
+}
 
+#[cfg(feature = "serialize_serde")]
+pub fn deserialize_message<'de, O, B>(buf: B) -> Result<SystemMessage<O>>
+where
+    O: Deserialize<'de>,
+    B: Buf,
+{
     #[cfg(feature = "serialize_serde_bincode")]
     { serde::bincode::deserialize_from_replica(buf) }
 
