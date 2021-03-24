87d7114ec7bb9f4c2353ef3c6f8921f3cda7281d
Tiago Carvalho <up201605259@fc.up.pt>
2021-03-24 22:15:52 +0000

    doc threadpool

--- src/bft/threadpool/mod.rs
+++ src/bft/threadpool/mod.rs
@@ -6,6 +6,12 @@ mod crossbeam;
 #[cfg(feature = "threadpool_cthpool")]
 mod cthpool;
 
+/// A thread pool type, used to run intensive CPU tasks.
+///
+/// The thread pool implements `Clone` with a cheap reference
+/// count increase operation. This means that if we drop its
+/// handle, the thread pool can continue to be used, as long
+/// as at least another instance of the original pool remains.
 #[derive(Clone)]
 pub struct ThreadPool {
     #[cfg(feature = "threadpool_crossbeam")]
@@ -15,6 +21,7 @@ pub struct ThreadPool {
     inner: cthpool::ThreadPool,
 }
 
+/// Helper type used to construct a new thread pool.
 pub struct Builder {
     #[cfg(feature = "threadpool_crossbeam")]
     inner: crossbeam::Builder,
@@ -24,6 +31,7 @@ pub struct Builder {
 }
 
 impl Builder {
+    /// Returns a new thread pool builder.
     pub fn new() -> Builder {
         let inner = {
             #[cfg(feature = "threadpool_crossbeam")]
@@ -35,11 +43,13 @@ impl Builder {
         Builder { inner }
     }
 
+    /// Returns the handle to a new thread pool.
     pub fn build(self) -> ThreadPool {
         let inner = self.inner.build();
         ThreadPool { inner }
     }
 
+    /// Configures the number of threads used by the thread pool.
     pub fn num_threads(self, num_threads: usize) -> Self {
         let inner = self.inner.num_threads(num_threads);
         Builder { inner }
@@ -49,6 +59,7 @@ impl Builder {
 }
 
 impl ThreadPool {
+    /// Spawns a new job into the thread pool.
     pub fn execute<F>(&self, job: F)
     where
         F: FnOnce() + Send + 'static,
@@ -56,6 +67,8 @@ impl ThreadPool {
         self.inner.execute(job)
     }
 
+    /// Synchronously waits for all the jobs queued in the pool
+    /// to complete.
     pub fn join(&self) {
         self.inner.join()
     }
