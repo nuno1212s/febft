222afe05c5a044af14272a042f193e1e4e058a79
Tiago Carvalho <up201605259@fc.up.pt>
2021-03-26 18:03:25 +0000

    finish hash

--- src/bft/crypto/hash/mod.rs
+++ src/bft/crypto/hash/mod.rs
@@ -3,6 +3,8 @@
 #[cfg(feature = "crypto_hash_ring_sha2")]
 mod ring_sha2;
 
+use crate::bft::error::*;
+
 /// The type `Context` represents an on-going hash digest calculation.
 pub struct Context {
     #[cfg(feature = "crypto_hash_ring_sha2")]
@@ -17,8 +19,59 @@ pub struct Digest {
     inner: ring_sha2::Digest,
 }
 
+impl Context {
+    /// Initializes a new `Context` instance.
+    ///
+    /// Feed this it data with `Context::update`.
+    pub fn new() -> Self {
+        let inner = {
+            #[cfg(feature = "crypto_hash_ring_sha2")]
+            { ring_sha2::Context::new() }
+        };
+        Context { inner }
+    }
+
+    /// Feeds the `Context` some data to be hashed.
+    pub fn update(&mut self, data: &[u8]) {
+        self.inner.update(data);
+    }
+
+    /// Extracts the resulting digest of hashing data onto the `Context`.
+    pub fn finish(self) -> Digest {
+        let inner = self.inner.finish();
+        Digest { inner }
+    }
+}
+
+impl Digest {
+    /// The length of the `Digest` in bytes.
+    pub const LENGTH: usize = {
+        #[cfg(feature = "crypto_hash_ring_sha2")]
+        { ring_sha2::Digest::LENGTH }
+    };
+
+    /// Constructs a `Digest` from a byte buffer of appropriate size.
+    pub fn from_bytes(raw_bytes: &[u8]) -> Result<Self> {
+        let inner = {
+            #[cfg(feature = "crypto_hash_ring_sha2")]
+            { ring_sha2::Digest::from_bytes(raw_bytes) }
+        }?;
+        Ok(Digest { inner })
+    }
+}
+
 impl AsRef<[u8]> for Digest {
     fn as_ref(&self) -> &[u8] {
         self.inner.as_ref()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Digest;
+
+    #[test]
+    fn test_length() {
+        assert_eq!(Digest::LENGTH, std::mem::size_of::<Digest>());
     }
 }
--- src/bft/crypto/hash/ring_sha2/mod.rs
+++ src/bft/crypto/hash/ring_sha2/mod.rs
@@ -6,11 +6,29 @@ use ring::digest::{
 
 use crate::bft::error::*;
 
-pub struct Context;
+pub struct Context {
+    inner: digest::Context,
+}
 
 #[derive(Copy, Clone)]
 #[repr(transparent)]
 pub struct Digest([u8; Digest::LENGTH]);
+
+impl Context {
+    pub fn new() -> Self {
+        let inner = digest::Context::new(&SHA256);
+        Context { inner }
+    }
+
+    pub fn update(&mut self, data: &[u8]) {
+        self.inner.update(data);
+    }
+
+    pub fn finish(self) -> Digest {
+        let h = self.inner.finish();
+        Digest::from_bytes_unchecked(h.as_ref())
+    }
+}
 
 impl Digest {
     pub const LENGTH: usize = SHA256_OUTPUT_LEN;
