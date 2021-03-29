3b646ff2d2a5d4fd5334aea982724a6ce9aad221
Tiago Carvalho <up201605259@fc.up.pt>
2021-03-29 16:52:08 +0100

    begin logger

--- src/bft/history/mod.rs
+++ src/bft/history/mod.rs
@@ -10,6 +10,7 @@ use crate::bft::communication::channel::MessageChannelTx;
 use crate::bft::communication::message::{
     Header,
     SystemMessage,
+    ConsensusMessageKind,
 };
 
 struct StoredMessage<O> {
@@ -18,7 +19,6 @@ struct StoredMessage<O> {
 }
 
 /// Represents a log of messages received by the BFT system.
-// TODO: garbage collect the log
 pub struct Log<O> {
     pre_prepares: VecDeque<StoredMessage<O>>,
     prepares: VecDeque<StoredMessage<O>>,
@@ -26,16 +26,42 @@ pub struct Log<O> {
     // others: ...
 }
 
+// TODO: garbage collect the log
+impl<O> Log<O> {
+    /// Creates a new message log.
+    pub fn new() -> Self {
+        Self {
+            pre_prepares: VecDeque::new(),
+            prepares: VecDeque::new(),
+            commits: VecDeque::new(),
+        }
+    }
+
+    /// Adds a new `message` and its respective `header` to the log.
+    pub fn insert(&mut self, header: Header, message: SystemMessage<O>) {
+        let message = StoredMessage { header, message };
+        if let SystemMessage::Consensus(ref m) = &message.message {
+            match m.kind {
+                ConsensusMessageKind::PrePrepare(_) => self.pre_prepares.push_back(message),
+                ConsensusMessageKind::Prepare => self.prepares.push_back(message),
+                ConsensusMessageKind::Commit => self.commits.push_back(message),
+            }
+        }
+    }
+}
+
 /// Represents an async message logging task.
 pub struct Logger<O> {
+    // handle used to receive messages to be logged
+    my_rx: ChannelRx<(Header, SystemMessage)>,
     // handle to the master channel used by the `System`
     system_tx: MessageChannelTx<O>,
     // the message log itself
     log: Log<O>,
 }
 
-//impl Logger {
-//    pub fn new(tx: MessageChannelTx) -> Self {
-//        ads
-//    }
-//}
+impl Logger {
+    pub fn new(tx: MessageChannelTx) -> Self {
+        unimplemented!()
+    }
+}
