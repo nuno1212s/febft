86e56177d75f4662fa7178574a5fcaa9e5c6e479
Tiago Carvalho <up201605259@fc.up.pt>
2021-03-30 16:53:53 +0100

    fix init

--- src/bft/mod.rs
+++ src/bft/mod.rs
@@ -12,11 +12,16 @@ pub mod error;
 
 use error::*;
 
+/// Configure the init process of the library.
+pub struct InitConfig {
+    pub async_threads: usize,
+}
+
 /// Initializes global data.
 ///
 /// Should always be called before other methods, otherwise runtime
 /// panics may ensue.
-pub fn init() -> Result<()> {
-    async_runtime::init()?;
+pub fn init(c: InitConfig) -> Result<()> {
+    async_runtime::init(c.async_threads)?;
     Ok(())
 }
