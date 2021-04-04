5db2f888f2b210c441c2e43f244399e44b89d464
Tiago Carvalho <up201605259@fc.up.pt>
2021-04-04 05:49:12 +0100

    simplify code

--- src/bft/communication/serialize/mod.rs
+++ src/bft/communication/serialize/mod.rs
@@ -1,7 +1,7 @@
 //! This module is responsible for serializing wire messages in `febft`.
 //!
 //! If using the [Cap'n'Proto](https://capnproto.org/capnp-tool.html) backend,
-//! the API for the module will be slightly different. Users can opt to enable
+//! users are expected to implement their own schema. Users can opt to enable
 //! the [serde](https://serde.rs/) backend instead, which has a much more flexible
 //! API, but performs worse, in general, because it doesn't have a
 //! zero-copy architecture, like Cap'n'Proto.
@@ -29,23 +29,17 @@ use crate::bft::communication::message::SystemMessage;
 #[cfg(feature = "serialize_capnp")]
 pub use self::capnp::{ToCapnp, FromCapnp};
 
-/// Serialize a wire message into the writer `W`.
-///
-/// Once the operation is finished, the buffer is returned.
-#[cfg(feature = "serialize_capnp")]
-pub fn serialize_message<O: ToCapnp, W: Write>(w: W, m: SystemMessage<O>) -> Result<W> {
-    capnp::serialize_message(w, m)
-}
-
 /// Serialize a wire message into the write `W`.
 ///
 /// Once the operation is finished, the buffer is returned.
-#[cfg(feature = "serialize_serde")]
 pub fn serialize_message<O, W>(w: W, m: SystemMessage<O>) -> Result<W>
 where
-    O: Serialize,
+    O: Marshal,
     W: Write,
 {
+    #[cfg(feature = "serialize_capnp")]
+    { capnp::serialize_message(w, m) }
+
     #[cfg(feature = "serialize_serde_bincode")]
     { serde::bincode::serialize_message(w, m) }
 
@@ -56,19 +50,15 @@ where
     { serde::cbor::serialize_message(w, m) }
 }
 
-/// Deserialize a wire message from a read `B`.
-#[cfg(feature = "serialize_capnp")]
-pub fn deserialize_message<O: FromCapnp, R: Read>(r: R) -> Result<SystemMessage<O>> {
-    capnp::deserialize_message(r)
-}
-
 /// Deserialize a wire message from a reader `R`.
-#[cfg(feature = "serialize_serde")]
 pub fn deserialize_message<O, R>(r: R) -> Result<SystemMessage<O>>
 where
-    O: for<'de> Deserialize<'de>,
+    O: Unmarshal,
     R: Read,
 {
+    #[cfg(feature = "serialize_capnp")]
+    { capnp::deserialize_message(r) }
+
     #[cfg(feature = "serialize_serde_bincode")]
     { serde::bincode::deserialize_message(r) }
 
