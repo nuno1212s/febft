87ab5639bf2d36dceb9cdbf30ceabea052dcd1fb
Tiago Carvalho <up201605259@fc.up.pt>
2021-04-05 18:56:14 +0100

    wip

--- examples/node-bootstrap.rs
+++ examples/node-bootstrap.rs
@@ -1,6 +1,11 @@
 use std::net::SocketAddr;
 
 use futures::io::{AsyncReadExt, AsyncWriteExt};
+use rustls::{
+    ServerConfig,
+    ClientConfig,
+    NoClientAuth,
+};
 
 use febft::bft::async_runtime as rt;
 use febft::bft::{
@@ -12,6 +17,22 @@ use febft::bft::communication::{
     NodeId,
     NodeConfig,
 };
+use febft::crypto::signature::{
+    KeyPair,
+    PublicKey,
+};
+use febft::error::*;
+
+macro_ruls! addr {
+    ($a:expr) => {{
+        if let Some((name, _)) = $a.split_once(':') {
+            let addr: SocketAddr = $a.parse().unwrap();
+            (addr, String::from(name))
+        } else {
+            unreachable!()
+        }
+    }}
+}
 
 macro_rules! map {
     ( $($key:expr => $value:expr),+ ) => {{
@@ -31,10 +52,37 @@ fn main() {
     rt::block_on(async_main());
 }
 
-async fn async_main() {
-    println!("test");
-    //let conf = NodeConfig {
-    //    f: 1,
-    //    id: NodeId::from(0),
-    //};
+async fn setup_node(id: NodeId, sk: KeyPair) -> Result<Node> {
+    // for testing purposes, turn off TLS auth
+    let server_config = get_server_config(false);
+    let client_config = get_client_config(false);
+
+    // build the node conf
+    let conf = NodeConfig {
+        id,
+        f: 1,
+        addrs: map! {
+            NodeId::from(0) => addr!("127.0.0.1:10000"),
+            NodeId::from(1) => addr!("127.0.0.1:10001"),
+            NodeId::from(2) => addr!("127.0.0.1:10002"),
+            NodeId::from(3) => addr!("127.0.0.1:10003"),
+        },
+        server_config,
+        client_config,
+    };
+
+    Node::bootstrap(conf).map(|(n, _)| n)
+}
+
+fn generate_keys() -> impl Iterator<Item = KeyPair> {
+    asd
+}
+
+fn get_server_config(_auth: bool) -> ServerConfig {
+    // TODO: implement TLS auth
+    ServerConfig::new(NoClientAuth::new())
+}
+
+fn get_client_config(_auth: bool) -> ServerConfig {
+    ClientConfig::new()
 }
