aac93ac21bc3d24e5a8f8426e4d42fc4bc0443a1
Tiago Carvalho <up201605259@fc.up.pt>
2021-04-05 19:21:10 +0100

    wip

--- examples/node-bootstrap.rs
+++ examples/node-bootstrap.rs
@@ -1,10 +1,15 @@
 use std::net::SocketAddr;
+use std::collections::HashMap;
 
 use futures::io::{AsyncReadExt, AsyncWriteExt};
 use rustls::{
     ServerConfig,
     ClientConfig,
     NoClientAuth,
+};
+use rand_core::{
+    OsRng,
+    RngCore,
 };
 
 use febft::bft::async_runtime as rt;
@@ -17,13 +22,13 @@ use febft::bft::communication::{
     NodeId,
     NodeConfig,
 };
-use febft::crypto::signature::{
+use febft::bft::crypto::signature::{
     KeyPair,
     PublicKey,
 };
-use febft::error::*;
+use febft::bft::error::*;
 
-macro_ruls! addr {
+macro_rules! addr {
     ($a:expr) => {{
         if let Some((name, _)) = $a.split_once(':') {
             let addr: SocketAddr = $a.parse().unwrap();
@@ -52,7 +57,21 @@ fn main() {
     rt::block_on(async_main());
 }
 
-async fn setup_node(id: NodeId, sk: KeyPair) -> Result<Node> {
+async fn async_main() {
+    let sk: HashMap<NodeId, KeyPair> = sk_stream()
+        .enumerate()
+        .map(|(id, sk)| (NodeId::from(id), sk))
+        .collect();
+    let pk: HashMap<NodeId, PublicKey> = sk
+        .iter()
+        .map(|(id, sk)| (*id, sk.public_key().into()))
+        .collect();
+
+    println!("{:#?}", sk);
+    println!("{:#?}", pk);
+}
+
+async fn setup_node(id: NodeId, sk: KeyPair, pk: HashMap<NodeId, PublicKey>) -> Result<Node<()>> {
     // for testing purposes, turn off TLS auth
     let server_config = get_server_config(false);
     let client_config = get_client_config(false);
@@ -67,15 +86,24 @@ async fn setup_node(id: NodeId, sk: KeyPair) -> Result<Node> {
             NodeId::from(2) => addr!("127.0.0.1:10002"),
             NodeId::from(3) => addr!("127.0.0.1:10003"),
         },
+        sk,
+        pk,
         server_config,
         client_config,
     };
 
-    Node::bootstrap(conf).map(|(n, _)| n)
+    Node::bootstrap(conf).await.map(|(n, _)| n)
 }
 
-fn generate_keys() -> impl Iterator<Item = KeyPair> {
-    asd
+fn sk_stream() -> impl Iterator<Item = KeyPair> {
+    std::iter::repeat_with(|| {
+        // only valid for ed25519!
+        let mut buf = [0; 32];
+
+        // gen key
+        OsRng.fill_bytes(&mut buf[..]);
+        KeyPair::from_bytes(&buf[..]).unwrap()
+    })
 }
 
 fn get_server_config(_auth: bool) -> ServerConfig {
@@ -83,6 +111,6 @@ fn get_server_config(_auth: bool) -> ServerConfig {
     ServerConfig::new(NoClientAuth::new())
 }
 
-fn get_client_config(_auth: bool) -> ServerConfig {
+fn get_client_config(_auth: bool) -> ClientConfig {
     ClientConfig::new()
 }
