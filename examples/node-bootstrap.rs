1983a7f3458cb86a7e48dc3f90ab82b1d16b4039
Tiago Carvalho <up201605259@fc.up.pt>
2021-04-05 22:43:22 +0100

    parallel read certs

--- examples/node-bootstrap.rs
+++ examples/node-bootstrap.rs
@@ -15,6 +15,10 @@ use rand_core::{
 };
 
 use febft::bft::async_runtime as rt;
+use febft::bft::threadpool::{
+    self,
+    ThreadPool,
+};
 use febft::bft::{
     init,
     InitConfig,
@@ -31,9 +35,9 @@ use febft::bft::crypto::signature::{
 use febft::bft::error::*;
 
 macro_rules! addr {
-    ($a:expr) => {{
+    ($h:expr => $a:expr) => {{
         let addr: SocketAddr = $a.parse().unwrap();
-        (addr, String::from("example.com"))
+        (addr, String::from($h))
     }}
 }
 
@@ -66,9 +70,14 @@ async fn async_main() {
         .map(|(id, sk)| (*id, sk.public_key().into()))
         .collect();
 
+    let pool = threadpool::Builder::new()
+        .num_threads(4)
+        .build();
+
     for id in NodeId::targets(0..4) {
         let sk = secret_keys.remove(&id).unwrap();
         let fut = setup_node(
+            pool.clone(),
             id,
             sk,
             public_keys.clone(),
@@ -78,25 +87,34 @@ async fn async_main() {
             println!("Spawn node #{}", usize::from(node.id()));
         });
     }
+    drop(pool);
 
     // wait 3 seconds then exit
     Delay::new(Duration::from_secs(3)).await;
 }
 
-async fn setup_node(id: NodeId, sk: KeyPair, pk: HashMap<NodeId, PublicKey>) -> Result<Node<()>> {
+async fn setup_node(
+    t: ThreadPool,
+    id: NodeId,
+    sk: KeyPair,
+    pk: HashMap<NodeId, PublicKey>,
+) -> Result<Node<()>> {
     // for testing purposes, turn off TLS auth
-    let server_config = get_server_config(false);
-    let client_config = get_client_config(false);
+    let (client_config, server_config) = {
+        let cli = get_client_config(&t, id);
+        let srv = get_server_config(&t, id);
+        futures::join!(cli, srv)
+    };
 
     // build the node conf
     let conf = NodeConfig {
         id,
         f: 1,
         addrs: map! {
-            NodeId::from(0u32) => addr!("127.0.0.1:10000"),
-            NodeId::from(1u32) => addr!("127.0.0.1:10001"),
-            NodeId::from(2u32) => addr!("127.0.0.1:10002"),
-            NodeId::from(3u32) => addr!("127.0.0.1:10003")
+            NodeId::from(0u32) => addr!("cop01" => "127.0.0.1:10001"),
+            NodeId::from(1u32) => addr!("cop02" => "127.0.0.1:10002"),
+            NodeId::from(2u32) => addr!("cop03" => "127.0.0.1:10003"),
+            NodeId::from(3u32) => addr!("cop04" => "127.0.0.1:10004")
         },
         sk,
         pk,
@@ -118,11 +136,22 @@ fn sk_stream() -> impl Iterator<Item = KeyPair> {
     })
 }
 
-fn get_server_config(_auth: bool) -> ServerConfig {
-    // TODO: implement TLS auth
-    ServerConfig::new(NoClientAuth::new())
+async fn get_server_config(t: &ThreadPool, id: NodeId) -> ServerConfig {
+    let (tx, rx) = oneshot::channel();
+    t.execute(move || {
+        let cfg = ServerConfig::new(NoClientAuth::new());
+
+        tx.send(cfg).unwrap();
+    });
+    rx.await.unwrap()
 }
 
-fn get_client_config(_auth: bool) -> ClientConfig {
-    ClientConfig::new()
+async fn get_client_config(t: &ThreadPool, id: NodeId) -> ClientConfig {
+    let (tx, rx) = oneshot::channel();
+    t.execute(move || {
+        let cfg = ClientConfig::new();
+
+        tx.send(cfg).unwrap();
+    });
+    rx.await.unwrap()
 }
