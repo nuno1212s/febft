4e88ffc75b9dcf47c642e529d9173fbef9be8db2
Tiago Carvalho <up201605259@fc.up.pt>
2021-04-09 00:53:17 +0100

    remove unused stuff

--- src/bft/communication/serialize/serde/mod.rs
+++
@@ -1,8 +0,0 @@
-#[cfg(feature = "serialize_serde_bincode")]
-pub mod bincode;
-
-#[cfg(feature = "serialize_serde_messagepack")]
-pub mod messagepack;
-
-#[cfg(feature = "serialize_serde_cbor")]
-pub mod cbor;
