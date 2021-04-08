d3886da01ab3cdabd62838cc0f46f8850c6b806c
Tiago Carvalho <up201605259@fc.up.pt>
2021-04-08 01:06:45 +0100

    separate code

---
+++ examples/common.rs
@@ -0,0 +1,163 @@
+use std::fs::File;
+use std::io::BufReader;
+use std::time::Duration;
+use std::net::SocketAddr;
+use std::collections::HashMap;
+
+use rustls::{
+    internal::pemfile,
+    ServerConfig,
+    ClientConfig,
+    RootCertStore,
+    AllowAnyAuthenticatedClient,
+};
+use rand_core::{
+    OsRng,
+    RngCore,
+};
+
+use febft::bft::error::*;
+use febft::bft::threadpool::ThreadPool;
+use febft::bft::communication::message::Message;
+use febft::bft::communication::{
+    Node,
+    NodeId,
+    NodeConfig,
+};
+use febft::bft::crypto::signature::{
+    KeyPair,
+    PublicKey,
+};
+
+#[macro_export]
+macro_rules! addr {
+    ($h:expr => $a:expr) => {{
+        let addr: ::std::net::SocketAddr = $a.parse().unwrap();
+        (addr, String::from($h))
+    }}
+}
+
+#[macro_export]
+macro_rules! map {
+    ( $($key:expr => $value:expr),+ ) => {{
+        let mut m = ::std::collections::HashMap::new();
+        $(
+            m.insert($key, $value);
+        )+
+        m
+     }};
+}
+
+pub async fn setup_node(
+    t: ThreadPool,
+    id: NodeId,
+    sk: KeyPair,
+    addrs: HashMap<NodeId, (SocketAddr, String)>,
+    pk: HashMap<NodeId, PublicKey>,
+) -> Result<(Node<()>, Vec<Message<()>>)> {
+    // read TLS configs concurrently
+    let (client_config, server_config) = {
+        let cli = get_client_config(&t, id);
+        let srv = get_server_config(&t, id);
+        futures::join!(cli, srv)
+    };
+
+    // build the node conf
+    let conf = NodeConfig {
+        id,
+        n: 4,
+        f: 1,
+        sync: Duration::from_millis(100),
+        sk,
+        pk,
+        addrs,
+        client_config,
+        server_config,
+    };
+
+    Node::bootstrap(conf).await
+}
+
+pub fn sk_stream() -> impl Iterator<Item = KeyPair> {
+    std::iter::repeat_with(|| {
+        // only valid for ed25519!
+        let mut buf = [0; 32];
+
+        // gen key
+        OsRng.fill_bytes(&mut buf[..]);
+        KeyPair::from_bytes(&buf[..]).unwrap()
+    })
+}
+
+async fn get_server_config(t: &ThreadPool, id: NodeId) -> ServerConfig {
+    let (tx, rx) = oneshot::channel();
+    t.execute(move || {
+        let id = usize::from(id) + 1;
+        let mut root_store = RootCertStore::empty();
+
+        // read ca file
+        let certs = {
+            let mut file = open_file("./ca-root/root.crt");
+            pemfile::certs(&mut file).expect("root cert")
+        };
+        root_store.add(&certs[0]).unwrap();
+
+        // create server conf
+        let auth = AllowAnyAuthenticatedClient::new(root_store);
+        let mut cfg = ServerConfig::new(auth);
+
+        // configure our cert chain and secret key
+        let sk = {
+            let mut file = open_file(&format!("./ca-root/cop0{}/cop0{}.key", id, id));
+            let mut sk = pemfile::rsa_private_keys(&mut file).expect("secret key");
+            sk.remove(0)
+        };
+        let chain = {
+            let mut file = open_file(&format!("./ca-root/cop0{}/cop0{}.crt", id, id));
+            let mut c = pemfile::certs(&mut file).expect("cop cert");
+            c.extend(certs);
+            c
+        };
+        cfg.set_single_cert(chain, sk).unwrap();
+
+        tx.send(cfg).unwrap();
+    });
+    rx.await.unwrap()
+}
+
+async fn get_client_config(t: &ThreadPool, id: NodeId) -> ClientConfig {
+    let (tx, rx) = oneshot::channel();
+    t.execute(move || {
+        let id = usize::from(id) + 1;
+        let mut cfg = ClientConfig::new();
+
+        // configure ca file
+        let certs = {
+            let mut file = open_file("./ca-root/root.crt");
+            pemfile::certs(&mut file).expect("root cert")
+        };
+        cfg.root_store.add(&certs[0]).unwrap();
+
+        // configure our cert chain and secret key
+        let sk = {
+            let mut file = open_file(&format!("./ca-root/cop0{}/cop0{}.key", id, id));
+            let mut sk = pemfile::rsa_private_keys(&mut file).expect("secret key");
+            sk.remove(0)
+        };
+        let chain = {
+            let mut file = open_file(&format!("./ca-root/cop0{}/cop0{}.crt", id, id));
+            let mut c = pemfile::certs(&mut file).expect("cop cert");
+            c.extend(certs);
+            c
+        };
+        cfg.set_single_client_cert(chain, sk).unwrap();
+
+        tx.send(cfg).unwrap();
+    });
+    rx.await.unwrap()
+}
+
+fn open_file(path: &str) -> BufReader<File> {
+    let file = File::open(path).expect(path);
+    BufReader::new(file)
+}
--- examples/node-bootstrap.rs
+++ examples/node-bootstrap.rs
@@ -1,39 +1,20 @@
-use std::fs::File;
-use std::io::BufReader;
+mod common;
+
+use common::*;
+
 use std::time::Duration;
-use std::net::SocketAddr;
 use std::collections::HashMap;
 
 use futures_timer::Delay;
-use rustls::{
-    internal::pemfile,
-    ServerConfig,
-    ClientConfig,
-    RootCertStore,
-    AllowAnyAuthenticatedClient,
-};
-use rand_core::{
-    OsRng,
-    RngCore,
-};
 
-use febft::bft::error::*;
+use febft::bft::threadpool;
+use febft::bft::communication::NodeId;
 use febft::bft::async_runtime as rt;
-use febft::bft::threadpool::{
-    self,
-    ThreadPool,
-};
 use febft::bft::{
     init,
     InitConfig,
 };
-use febft::bft::communication::{
-    Node,
-    NodeId,
-    NodeConfig,
-};
 use febft::bft::communication::message::{
-    Message,
     SystemMessage,
     RequestMessage,
 };
@@ -41,23 +22,6 @@ use febft::bft::crypto::signature::{
     KeyPair,
     PublicKey,
 };
-
-macro_rules! addr {
-    ($h:expr => $a:expr) => {{
-        let addr: SocketAddr = $a.parse().unwrap();
-        (addr, String::from($h))
-    }}
-}
-
-macro_rules! map {
-    ( $($key:expr => $value:expr),+ ) => {{
-        let mut m = ::std::collections::HashMap::new();
-        $(
-            m.insert($key, $value);
-        )+
-        m
-     }};
-}
 
 fn main() {
     let conf = InitConfig {
@@ -83,11 +47,18 @@ async fn async_main() {
         .build();
 
     for id in NodeId::targets(0..4) {
+        let addrs= map! {
+            NodeId::from(0u32) => addr!("cop01" => "127.0.0.1:10001"),
+            NodeId::from(1u32) => addr!("cop02" => "127.0.0.1:10002"),
+            NodeId::from(2u32) => addr!("cop03" => "127.0.0.1:10003"),
+            NodeId::from(3u32) => addr!("cop04" => "127.0.0.1:10004")
+        };
         let sk = secret_keys.remove(&id).unwrap();
         let fut = setup_node(
             pool.clone(),
             id,
             sk,
+            addrs,
             public_keys.clone(),
         );
         rt::spawn(async move {
@@ -106,122 +77,4 @@ async fn async_main() {
 
     // wait 3 seconds then exit
     Delay::new(Duration::from_secs(3)).await;
-}
-
-async fn setup_node(
-    t: ThreadPool,
-    id: NodeId,
-    sk: KeyPair,
-    pk: HashMap<NodeId, PublicKey>,
-) -> Result<(Node<()>, Vec<Message<()>>)> {
-    // read TLS configs concurrently
-    let (client_config, server_config) = {
-        let cli = get_client_config(&t, id);
-        let srv = get_server_config(&t, id);
-        futures::join!(cli, srv)
-    };
-
-    // build the node conf
-    let conf = NodeConfig {
-        id,
-        n: 4,
-        f: 1,
-        sync: Duration::from_millis(100),
-        addrs: map! {
-            NodeId::from(0u32) => addr!("cop01" => "127.0.0.1:10001"),
-            NodeId::from(1u32) => addr!("cop02" => "127.0.0.1:10002"),
-            NodeId::from(2u32) => addr!("cop03" => "127.0.0.1:10003"),
-            NodeId::from(3u32) => addr!("cop04" => "127.0.0.1:10004")
-        },
-        sk,
-        pk,
-        client_config,
-        server_config,
-    };
-
-    Node::bootstrap(conf).await
-}
-
-fn sk_stream() -> impl Iterator<Item = KeyPair> {
-    std::iter::repeat_with(|| {
-        // only valid for ed25519!
-        let mut buf = [0; 32];
-
-        // gen key
-        OsRng.fill_bytes(&mut buf[..]);
-        KeyPair::from_bytes(&buf[..]).unwrap()
-    })
-}
-
-async fn get_server_config(t: &ThreadPool, id: NodeId) -> ServerConfig {
-    let (tx, rx) = oneshot::channel();
-    t.execute(move || {
-        let id = usize::from(id) + 1;
-        let mut root_store = RootCertStore::empty();
-
-        // read ca file
-        let certs = {
-            let mut file = open_file("./ca-root/root.crt");
-            pemfile::certs(&mut file).expect("root cert")
-        };
-        root_store.add(&certs[0]).unwrap();
-
-        // create server conf
-        let auth = AllowAnyAuthenticatedClient::new(root_store);
-        let mut cfg = ServerConfig::new(auth);
-
-        // configure our cert chain and secret key
-        let sk = {
-            let mut file = open_file(&format!("./ca-root/cop0{}/cop0{}.key", id, id));
-            let mut sk = pemfile::rsa_private_keys(&mut file).expect("secret key");
-            sk.remove(0)
-        };
-        let chain = {
-            let mut file = open_file(&format!("./ca-root/cop0{}/cop0{}.crt", id, id));
-            let mut c = pemfile::certs(&mut file).expect("cop cert");
-            c.extend(certs);
-            c
-        };
-        cfg.set_single_cert(chain, sk).unwrap();
-
-        tx.send(cfg).unwrap();
-    });
-    rx.await.unwrap()
-}
-
-async fn get_client_config(t: &ThreadPool, id: NodeId) -> ClientConfig {
-    let (tx, rx) = oneshot::channel();
-    t.execute(move || {
-        let id = usize::from(id) + 1;
-        let mut cfg = ClientConfig::new();
-
-        // configure ca file
-        let certs = {
-            let mut file = open_file("./ca-root/root.crt");
-            pemfile::certs(&mut file).expect("root cert")
-        };
-        cfg.root_store.add(&certs[0]).unwrap();
-
-        // configure our cert chain and secret key
-        let sk = {
-            let mut file = open_file(&format!("./ca-root/cop0{}/cop0{}.key", id, id));
-            let mut sk = pemfile::rsa_private_keys(&mut file).expect("secret key");
-            sk.remove(0)
-        };
-        let chain = {
-            let mut file = open_file(&format!("./ca-root/cop0{}/cop0{}.crt", id, id));
-            let mut c = pemfile::certs(&mut file).expect("cop cert");
-            c.extend(certs);
-            c
-        };
-        cfg.set_single_client_cert(chain, sk).unwrap();
-
-        tx.send(cfg).unwrap();
-    });
-    rx.await.unwrap()
-}
-
-fn open_file(path: &str) -> BufReader<File> {
-    let file = File::open(path).expect(path);
-    BufReader::new(file)
 }
