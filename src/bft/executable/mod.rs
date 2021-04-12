f68cfb968536607f2e44943b523ddc056b8b99a2
Tiago Carvalho <up201605259@fc.up.pt>
2021-04-12 20:17:30 +0100

    queue a new execution request

--- src/bft/executable/mod.rs
+++ src/bft/executable/mod.rs
@@ -72,17 +72,17 @@ pub struct ExecutorHandle<S: Service> {
     my_tx: ChannelTx<ExecutionRequest<Request<S>>>,
 }
 
-//impl<S: Service> ExecutorHandle<S>
-//where
-//    S: Service + Send + 'static,
-//    Request<S>: Send + 'static,
-//    Reply<S>: Send + 'static,
-//{
-//    /// Adds a new `message` and its respective `header` to the log.
-//    pub async fn queue(&mut self, to: NodeId, req: ) -> Result<()> {
-//        self.my_tx.send(LogOperation::Insert(header, message)).await
-//    }
-//}
+impl<S: Service> ExecutorHandle<S>
+where
+    S: Service + Send + 'static,
+    Request<S>: Send + 'static,
+    Reply<S>: Send + 'static,
+{
+    /// Adds a new `message` and its respective `header` to the log.
+    pub async fn queue(&mut self, to: NodeId, sig: Signature, req: Request<S>) -> Result<()> {
+        self.my_tx.send(ExecutionRequest::ReadWrite(to, sig, req)).await
+    }
+}
 
 impl<S: Service> Clone for ExecutorHandle<S> {
     fn clone(&self) -> Self {
