2feefd1508f6976b8dcbd0899fa2f75847dbcafc
Tiago Carvalho <up201605259@fc.up.pt>
2021-04-15 17:45:18 +0100

    wip: client

--- src/bft/core/client/mod.rs
+++ src/bft/core/client/mod.rs
@@ -1,25 +1,25 @@
 //! Contains the client side core protocol logic of `febft`.
 
-//use std::sync::Arc;
-//
-//use futures::lock::Mutex;
-//
 //use super::SystemParams;
-//use crate::bft::communication::Node;
 //use crate::bft::collections::{self, HashMap};
-//use crate::bft::communication::serialize::SharedData;
-//
-//pub struct Client<D: SharedData> {
-//    node: Arc<Node<D>>,
-//}
-//
-//impl<D> Client<D>
-//where
-//    D: SharedData + 'static,
-//    D::Request: Send + 'static,
-//    D::Reply: Send + 'static,
-//{
-//    /// asd.
-//    async fn message_recv_task() -> Result<()> {
-//    }
-//}
+use crate::bft::communication::serialize::SharedData;
+use crate::bft::communication::{
+    Node,
+    SendNode,
+};
+
+pub struct Client<D: SharedData> {
+    node: SendNode<D>,
+}
+
+impl<D> Client<D>
+where
+    D: SharedData + 'static,
+    D::Request: Send + 'static,
+    D::Reply: Send + 'static,
+{
+    async fn message_recv_task(mut node: Node<D>) {
+        drop(node);
+        unimplemented!()
+    }
+}
