0eaf978078b9f961ddbbcc44c81c273cd6a39f58
Tiago Carvalho <up201605259@fc.up.pt>
2021-04-19 00:32:00 +0100

    from 32 to 64 bit nonces

--- src/bft/prng/mod.rs
+++ src/bft/prng/mod.rs
@@ -1,20 +1,20 @@
 //! Pseudo random number generator.
 //!
-//! The current implementation is based on [xoshiro128**](https://prng.di.unimi.it/xoshiro128starstar.c),
+//! The current implementation is based on [xoshiro256**](https://prng.di.unimi.it/xoshiro256starstar.c),
 //! from David Blackman and Sebastiano Vigna. This source code is a one-to-one translation of their
 //! C code, released to the public domain.
 
 use rand_core::{RngCore, OsRng};
 
-/// This type is a container for the 128-bit state of `xoshiro128**`.
+/// This type is a container for the 256-bit state of `xoshiro256**`.
 pub struct State {
-    s: [u32; 4],
+    s: [u64; 4],
 }
 
 impl State {
     /// Creates a new PRNG from a cryptographically secure random seed.
     pub fn new() -> Self {
-        let mut seed = [0; 16];
+        let mut seed = [0; 32];
         OsRng.fill_bytes(&mut seed);
 
         let s = unsafe { std::mem::transmute(seed) };
@@ -26,9 +26,9 @@ impl State {
 
     /// Returns a new 32-bit random number.
     #[inline]
-    pub fn next_state(&mut self) -> u32 {
+    fn next_state(&mut self) -> u64 {
         let result = rotl(self.s[1] * 5, 7) * 9;
-        let t = self.s[1] << 9;
+        let t = self.s[1] << 17;
 
         self.s[2] ^= self.s[0];
         self.s[3] ^= self.s[1];
@@ -37,14 +37,19 @@ impl State {
 
         self.s[2] ^= t;
 
-        self.s[3] = rotl(self.s[3], 11);
+        self.s[3] = rotl(self.s[3], 45);
 
         result
     }
 
     #[inline]
     fn long_jump(&mut self) {
-        const LONG_JUMP: [u32; 4] = [0xb523952e, 0x0b6f099f, 0xccf5a0ef, 0x1c580662];
+        const LONG_JUMP: [u64; 4] = [
+            0x76e15d3efefdcbbf,
+            0xc5004e441c522fb3,
+            0x77710069854ee241,
+            0x39109bb02acbe635,
+        ];
 
         let mut s0 = 0;
         let mut s1 = 0;
@@ -52,7 +57,7 @@ impl State {
         let mut s3 = 0;
 
         for &jmp_val in LONG_JUMP.iter() {
-            for b in 0..32 {
+            for b in 0..64 {
                 if jmp_val & 1 << b != 0 {
                     s0 ^= self.s[0];
                     s1 ^= self.s[1];
@@ -71,15 +76,15 @@ impl State {
 }
 
 impl Iterator for State {
-    type Item = u32;
+    type Item = u64;
 
     #[inline]
-    fn next(&mut self) -> Option<u32> {
+    fn next(&mut self) -> Option<u64> {
         Some(self.next_state())
     }
 }
 
 #[inline]
-fn rotl(x: u32, k: u32) -> u32 {
-	(x << k) | (x >> (32 - k))
+fn rotl(x: u64, k: u64) -> u64 {
+	(x << k) | (x >> (64 - k))
 }
