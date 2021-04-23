e96370e81758d73b0fdf0ae381d265e618a016ae
Tiago Carvalho <up201605259@fc.up.pt>
2021-04-23 20:11:24 +0100

    new collection type

--- src/bft/collections/mod.rs
+++ src/bft/collections/mod.rs
@@ -8,11 +8,19 @@ pub type RandomState = ::twox_hash::RandomXxh3HashBuilder64;
 #[cfg(feature = "collections_randomstate_std")]
 pub type RandomState = ::std::collections::hash_map::RandomState;
 
+/// A map which, as the name suggests, maintains the order of its `(K, V)` pairs.
+pub type OrderedMap<K, V> = ::linked_hash_map::LinkedHashMap<K, V, RandomState>;
+
 /// A `HashMap` with a faster hashing function.
 pub type HashMap<K, V> = ::std::collections::HashMap<K, V, RandomState>;
 
 /// A `HashSet` with a faster hashing function.
 pub type HashSet<T> = ::std::collections::HashSet<T, RandomState>;
+
+/// Creates a new `OrderedMap`.
+pub fn ordered_map<K: Eq + ::std::hash::Hash, V>() -> OrderedMap<K, V> {
+    OrderedMap::with_hasher(Default::default())
+}
 
 /// Creates a new `HashMap`.
 pub fn hash_map<K, V>() -> HashMap<K, V> {
