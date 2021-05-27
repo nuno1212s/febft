aa62bd4775ec236edbcd8d51c97776f50f6ed2ef
Tiago Carvalho <up201605259@fc.up.pt>
2021-05-27 15:01:39 +0100

    num cpus

--- examples/client-local.rs
+++ examples/client-local.rs
@@ -18,7 +18,7 @@ use febft::bft::crypto::signature::{
 
 fn main() {
     let conf = InitConfig {
-        async_threads: 4,
+        async_threads: num_cpus::get(),
     };
     let _guard = unsafe { init(conf).unwrap() };
     rt::block_on(async_main());
