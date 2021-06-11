a780c60458f81e6ad7f8a6b9ab292612f906d544
Tiago Carvalho <tiago.carvalho@fc.up.pt>
2021-06-11 15:49:42 +0100

    wait

--- examples/bootstrap-cop.rs
+++ examples/bootstrap-cop.rs
@@ -86,6 +86,10 @@ async fn async_main(id: NodeId) {
         node
     };
 
+    // wait a few seconds
+    println!("Waiting 10 seconds...");
+    Delay::new(Duration::from_secs(10)).await;
+
     // broadcast message
     let m = SystemMessage::Request(RequestMessage::new(Vec::new()));
     node.broadcast(m, NodeId::targets(0..4));
