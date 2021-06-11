ed1d658726ef6c69120fce12cdd551da7b9ee1b3
Tiago Carvalho <tiago.carvalho@fc.up.pt>
2021-06-11 15:12:07 +0100

    num cpus

--- examples/bootstrap-cop.rs
+++ examples/bootstrap-cop.rs
@@ -34,7 +34,7 @@ fn main() {
         .parse()
         .unwrap();
     let conf = InitConfig {
-        async_threads: 40,
+        async_threads: num_cpus::get(),
     };
     let _guard = unsafe { init(conf).unwrap() };
     rt::block_on(async_main(NodeId::from(id)));
