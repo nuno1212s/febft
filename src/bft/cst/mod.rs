c7cea746d8d9f5dc499d22e24229de7e22f74c7d
Tiago Carvalho <tiago.carvalho@fc.up.pt>
2021-06-21 14:56:33 +0100

    cst struct

--- src/bft/cst/mod.rs
+++ src/bft/cst/mod.rs
@@ -3,4 +3,12 @@
 //! The implementation is based on the paper «On the Efﬁciency of
 //! Durable State Machine Replication», by A. Bessani et al.
 
+/// Represents the state of an on-going colloborative
+/// state transfer protocol execution.
+pub struct CollabStTransfer {
+    phase: ProtoPhase,
+}
 
+enum ProtoPhase {
+    Init,
+}
