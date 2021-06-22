53c134a4cac027c7ac32cd3426929734af34075f
Tiago Carvalho <tiago.carvalho@fc.up.pt>
2021-06-22 16:14:18 +0100

    wip: cst

--- src/bft/cst/mod.rs
+++ src/bft/cst/mod.rs
@@ -3,17 +3,42 @@
 //! The implementation is based on the paper «On the Efﬁciency of
 //! Durable State Machine Replication», by A. Bessani et al.
 
-use crate::bft::prng::State as RngState;
+use crate::bft::communication::message::{
+    Header,
+    SystemMessage,
+    ConsensusMessage,
+    ConsensusMessageKind,
+};
 
 /// Represents the state of an on-going colloborative
 /// state transfer protocol execution.
 pub struct CollabStTransfer {
     phase: ProtoPhase,
-    rng: RngState,
+    seq: SeqNo,
+}
+
+/// Status returned from asking the latest sequence number
+/// the consensus protocol is currently ordering.
+pub enum ConsensusSeqStatus {
+    WaitingFetch,
+    Fetched(SeqNo),
 }
 
 enum ProtoPhase {
     Init,
     ReceivingCid(usize),
     ReceivingAppState(usize),
+}
+
+impl CollabStTransfer {
+    pub fn new() -> Self {
+        Self {
+            phase: ProtoPhase::Init,
+            seq: SeqNo::from(0),
+        }
+    }
+
+    pub fn process_message(&mut self, header: Header, message: 
+
+    pub fn ask_latest_consensus_seq_no(&mut self, node: &mut Node<S::Data>) 
 }
