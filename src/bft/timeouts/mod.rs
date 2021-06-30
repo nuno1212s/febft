1197bcddf71d8ad93c357cd5a6ede1141ed77234
Tiago Carvalho <tiago.carvalho@fc.up.pt>
2021-06-30 22:00:25 +0100

    wip: timeout resolver

--- src/bft/timeouts/mod.rs
+++ src/bft/timeouts/mod.rs
@@ -3,7 +3,9 @@
 //! This includes on-going client requests, as well as CST and
 //! view change messages exchanged between replicas.
 
-//use crate::bft::communication::message::Message;
+use std::marker::PhantomData;
+
+use crate::bft::async_runtime as rt;
 use crate::bft::communication::channel::{
     self,
     ChannelTx,
@@ -17,15 +19,43 @@ use crate::bft::executable::{
     State,
 };
 
-pub enum TimeoutRequest {
+pub enum Timeout {
     // TODO: fill in some items here
 }
 
+enum TimeoutKind {
+    Request(Timeout),
+    Reply(Timeout),
+}
+
 pub struct TimeoutsHandle {
-    tx: (),
+    tx: ChannelTx<TimeoutKind>,
 }
 
 pub struct Timeouts<S: Service> {
-    rx: ChannelRx<TimeoutRequest>,
-    system_tx: MessageChannelTx<State<S>, Request<S>, Reply<S>>,
+    _marker: PhantomData<S>,
+}
+
+impl<S: Service> Timeouts<S> {
+    const CHAN_BOUND: usize = 128;
+
+    pub fn new(
+        granularity: Duration,
+        system_tx: MessageChannelTx<State<S>, Request<S>, Reply<S>>,
+    ) -> TimeoutsHandle {
+        let (handler_tx, handler_rx) = channel::new_bounded(Self::CHAN_BOUND);
+        let (resolver_tx, resolver_rx) = channel::new_bounded(Self::CHAN_BOUND);
+
+        rt::spawn(async move {
+            asd
+        });
+        rt::spawn(async move {
+            while let Ok(request) = handler_rx.recv().await {
+                // TODO: handle timeouts
+                drop(request);
+            }
+        });
+
+        TimeoutsHandle { tx: handler_tx }
+    }
 }
