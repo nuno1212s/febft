fde42e5673deeddfe370842346c31d91409ed80d
Tiago Carvalho <tiago.carvalho@fc.up.pt>
2021-06-30 21:49:51 +0100

    wip: begin timeouts mod

--- src/bft/timeouts/mod.rs
+++ src/bft/timeouts/mod.rs
@@ -3,6 +3,29 @@
 //! This includes on-going client requests, as well as CST and
 //! view change messages exchanged between replicas.
 
+//use crate::bft::communication::message::Message;
+use crate::bft::communication::channel::{
+    self,
+    ChannelTx,
+    ChannelRx,
+    MessageChannelTx,
+};
+use crate::bft::executable::{
+    Service,
+    Request,
+    Reply,
+    State,
+};
+
+pub enum TimeoutRequest {
+    // TODO: fill in some items here
+}
+
 pub struct TimeoutsHandle {
     tx: (),
+}
+
+pub struct Timeouts<S: Service> {
+    rx: ChannelRx<TimeoutRequest>,
+    system_tx: MessageChannelTx<State<S>, Request<S>, Reply<S>>,
 }
