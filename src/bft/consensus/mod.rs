ee34735ead212bb822bea611dced03345d0a6784
Tiago Carvalho <tiago.carvalho@fc.up.pt>
2021-07-06 22:16:16 +0100

    add view

--- src/bft/consensus/mod.rs
+++ src/bft/consensus/mod.rs
@@ -267,6 +267,7 @@ where
     /// received from peer nodes in the CST protocol.
     pub fn install_new_phase(
         &mut self,
+        view: ViewInfo,
         recovery_state: &RecoveryState<State<S>, Request<S>>,
     ) {
         // get the latest seq no
