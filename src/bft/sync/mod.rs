a681082da32e46e61d0d170f823339712e6a71eb
Tiago Carvalho <tiago.carvalho@fc.up.pt>
2021-07-08 16:14:55 +0100

    proto phases

--- src/bft/sync/mod.rs
+++ src/bft/sync/mod.rs
@@ -3,6 +3,13 @@
 //! This code allows a replica to change its view, where a new
 //! leader is elected.
 
+enum ProtoPhase {
+    Init,
+    Stopping,
+    StoppingData,
+    Syncing,
+}
+
 // TODO:
 // - the fields in this struct
 // - TboQueue for sync phase messages?
