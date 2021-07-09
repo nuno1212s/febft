7a278a4c207cefc2e6dfa7422a5aa2d6444d9b2a
Tiago Carvalho <tiago.carvalho@fc.up.pt>
2021-07-09 16:43:34 +0100

    wip: process view change msg and client request time out

--- src/bft/sync/mod.rs
+++ src/bft/sync/mod.rs
@@ -10,7 +10,37 @@ enum ProtoPhase {
     Syncing,
 }
 
+pub enum SynchronizerStatus {
+    // TODO: statuses returned from `process_message`
+}
+
 // TODO:
 // - the fields in this struct
 // - TboQueue for sync phase messages?
 pub struct Synchronizer;
+
+impl Synchronizer {
+/*
+    /// Advances the state of the view change state machine.
+    pub fn process_message(
+        &mut self,
+        header: Header,
+        message: ViewChangeMessage,
+        view: ViewInfo,
+        log: &mut Log<State<S>, Request<S>, Reply<S>>,
+        node: &mut Node<S::Data>,
+    ) -> SynchronizerStatus {
+        unimplemented!()
+    }
+*/
+
+    /// Handle a timeout received from the timeouts layer.
+    pub fn timed_out(&mut self) -> SynchronizerStatus {
+        // TODO:
+        // - on the first timeout we forward pending requests to
+        //   the leader
+        // - on the second timeout, we start a view change by
+        //   broadcasting a STOP message
+        unimplemented!()
+    }
+}
