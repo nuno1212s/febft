01694d499b8981c58dbda1d55ec3bf7d44430cc1
Tiago Carvalho <tiago.carvalho@fc.up.pt>
2021-11-02 18:28:16 +0000

    use system time, similar to java

--- src/bft/benchmarks/mod.rs
+++ src/bft/benchmarks/mod.rs
@@ -1,10 +1,10 @@
-use std::time::Instant;
+use std::time::{Instant, SystemTime};
 
 pub struct RequestMeta {
-    reception_time: Instant,
-    consensus_start_time: Instant,
-    consensus_decision_time: Instant,
-    execution_time: Instant,
+    reception_time: SystemTime,
+    consensus_start_time: SystemTime,
+    consensus_decision_time: SystemTime,
+    execution_time: SystemTime,
 }
 
 pub struct BenchmarkHelper {
@@ -15,6 +15,8 @@ pub trait BenchmarkHelperStore {
     fn store(self, bench: &mut BenchmarkHelper);
 }
 
+// this code was more or less 1:1 translated from BFT-SMaRt,
+// even its oddities, such as cloning the values array
 impl BenchmarkHelper {
     pub fn new(capacity: usize) -> Self {
         Self {
@@ -37,6 +39,20 @@ impl BenchmarkHelper {
             .sum();
 
         (count as f64) / ((values.len() - 2*limit) as f64)
+    }
+}
+
+impl BenchmarkHelperStore for (SystemTime, SystemTime) {
+    fn store(self, bench: &mut BenchmarkHelper) {
+        const MAX: u128 = i64::MAX as u128;
+
+        let (start, end) = self;
+        let duration = end
+            .duration_since(start)
+            .expect("Non-monotonic time detected!")
+            .as_nanos();
+
+        bench.values.push((duration & MAX) as i64);
     }
 }
 
