ab084b05bdc1024cb178acf6e935b676a7cc804f
nunogneto <nuno.neto.g@gmail.com>
2022-02-10 17:52:32 +0000

    Got things compiling with the new custom channels.

--- src/bft/communication/channel/mod.rs
+++ src/bft/communication/channel/mod.rs
@@ -1,5 +1,28 @@
 //! FIFO channels used to send messages between async tasks.
 
+use std::collections::LinkedList;
+use std::future::Future;
+use std::pin::Pin;
+use std::sync::Arc;
+use std::task::{Context, Poll};
+
+use chrono::offset::Utc;
+use event_listener::Event;
+use futures::future::FusedFuture;
+use futures::select;
+use parking_lot::Mutex;
+
+use crate::bft::async_runtime as rt;
+use crate::bft::communication::message::{
+    ConsensusMessage,
+    Message,
+    ReplyMessage,
+    RequestMessage,
+    StoredMessage,
+    SystemMessage,
+};
+use crate::bft::error::*;
+
 #[cfg(feature = "channel_futures_mpsc")]
 mod futures_mpsc;
 
@@ -8,29 +31,6 @@ mod flume_mpmc;
 
 #[cfg(feature = "channel_async_channel_mpmc")]
 mod async_channel_mpmc;
-
-use std::pin::Pin;
-use std::sync::Arc;
-use std::future::Future;
-use std::task::{Poll, Context};
-use std::collections::LinkedList;
-
-use chrono::offset::Utc;
-use futures::select;
-use event_listener::Event;
-use futures::future::FusedFuture;
-use parking_lot::Mutex;
-
-use crate::bft::error::*;
-use crate::bft::async_runtime as rt;
-use crate::bft::communication::message::{
-    Message,
-    SystemMessage,
-    RequestMessage,
-    ReplyMessage,
-    StoredMessage,
-    ConsensusMessage,
-};
 
 /// General purpose channel's sending half.
 pub struct ChannelTx<T> {
@@ -42,6 +42,9 @@ pub struct ChannelTx<T> {
 
     #[cfg(feature = "channel_async_channel_mpmc")]
     inner: async_channel_mpmc::ChannelTx<T>,
+
+    #[cfg(feature = "channel_custom_dump")]
+    inner: DSRust::channels::queue_channel::ChannelTx<T, LFBRArrayQueue<T>>,
 }
 
 /// General purpose channel's receiving half.
@@ -54,6 +57,9 @@ pub struct ChannelRx<T> {
 
     #[cfg(feature = "channel_async_channel_mpmc")]
     inner: async_channel_mpmc::ChannelRx<T>,
+
+    #[cfg(feature = "channel_custom_dump")]
+    inner: DSRust::channels::queue_channel::ChannelRx<T, LFBRArrayQueue<T>>,
 }
 
 /// Future for a general purpose channel's receiving operation.
@@ -66,6 +72,9 @@ pub struct ChannelRxFut<'a, T> {
 
     #[cfg(feature = "channel_async_channel_mpmc")]
     inner: async_channel_mpmc::ChannelRxFut<'a, T>,
+
+    #[cfg(feature = "channel_custom_dump")]
+    inner: DSRust::channels::queue_channel::ChannelRxFut<'a, T, LFBRArrayQueue<T>>,
 }
 
 impl<T> Clone for ChannelTx<T> {
@@ -82,17 +91,22 @@ impl<T> Clone for ChannelTx<T> {
 pub fn new_bounded<T>(bound: usize) -> (ChannelTx<T>, ChannelRx<T>) {
     let (tx, rx) = {
         #[cfg(feature = "channel_futures_mpsc")]
-        { futures_mpsc::new_bounded(bound) }
+            { futures_mpsc::new_bounded(bound) }
 
         #[cfg(feature = "channel_flume_mpmc")]
-        { flume_mpmc::new_bounded(bound) }
+            { flume_mpmc::new_bounded(bound) }
 
         #[cfg(feature = "channel_async_channel_mpmc")]
-        { async_channel_mpmc::new_bounded(bound) }
+            { async_channel_mpmc::new_bounded(bound) }
+
+        #[cfg(feature = "channel_custom_dump")]
+            { DSRust::channels::queue_channel::bounded_lf_room_queue(bound) }
     };
-    let tx = ChannelTx { inner: tx };
-    let rx = ChannelRx { inner: rx };
-    (tx, rx)
+
+    let ttx = ChannelTx { inner: tx };
+    let rrx = ChannelRx { inner: rx };
+
+    (ttx, rrx)
 }
 
 impl<T> ChannelTx<T> {
@@ -136,7 +150,10 @@ pub enum MessageChannelTx<S, O, P> {
     },
     Server {
         other: ChannelTx<Message<S, O, P>>,
+        #[cfg(not(feature = "channel_custom_dump"))]
         requests: Arc<RequestBatcherShared<O>>,
+        #[cfg(feature = "channel_custom_dump")]
+        requests: ChannelTx<StoredMessage<RequestMessage<O>>>,
         consensus: ChannelTx<StoredMessage<ConsensusMessage<O>>>,
     },
 }
@@ -149,9 +166,228 @@ pub enum MessageChannelRx<S, O, P> {
     },
     Server {
         other: ChannelRx<Message<S, O, P>>,
+        #[cfg(not(feature = "channel_custom_dump"))]
         requests: ChannelRx<Vec<StoredMessage<RequestMessage<O>>>>,
+        #[cfg(feature = "channel_custom_dump")]
+        requests: ChannelRx<StoredMessage<RequestMessage<O>>>,
         consensus: ChannelRx<StoredMessage<ConsensusMessage<O>>>,
     },
+}
+
+/// Creates a new channel that can queue up to `bound` messages
+/// from different async senders.
+pub fn new_message_channel<S, O, P>(
+    client: bool,
+    bound: usize,
+) -> (MessageChannelTx<S, O, P>, MessageChannelRx<S, O, P>, Option<RequestBatcher<O>>) {
+    if client {
+        let (rr_tx, rr_rx) = new_bounded(bound);
+        let (o_tx, o_rx) = new_bounded(bound);
+
+        let tx = MessageChannelTx::Client { replies: rr_tx, other: o_tx };
+        let rx = MessageChannelRx::Client { replies: rr_rx, other: o_rx };
+
+        (tx, rx, None)
+    } else {
+        let (c_tx, c_rx) = new_bounded(bound);
+        let (o_tx, o_rx) = new_bounded(bound);
+
+        let (reqbatch_tx, reqbatch_rx) = new_bounded(bound);
+
+        let batcher;
+
+        let mut shared: Option<Arc<RequestBatcherShared<O>>> = None;
+
+        #[cfg(not(feature = "channel_custom_dump"))]
+            {
+                shared = Some(Arc::new(RequestBatcherShared {
+                    event: Event::new(),
+                    batch: Mutex::new(LinkedList::new()),
+                }));
+
+                batcher = Some(RequestBatcher {
+                    requests: Arc::clone(shared.as_ref().unwrap()),
+                    to_core_server_task: reqbatch_tx,
+                });
+            }
+
+        #[cfg(feature = "channel_custom_dump")]
+            { batcher = None; }
+
+        let tx = MessageChannelTx::Server {
+            consensus: c_tx,
+            #[cfg(not(feature = "channel_custom_dump"))]
+            requests: shared.unwrap(),
+            #[cfg(feature = "channel_custom_dump")]
+            requests: reqbatch_tx,
+            other: o_tx,
+        };
+
+        let rx = MessageChannelRx::Server {
+            consensus: c_rx,
+            requests: reqbatch_rx,
+            other: o_rx,
+        };
+
+        (tx, rx, batcher)
+    }
+}
+
+impl<S, O, P> Clone for MessageChannelTx<S, O, P> {
+    fn clone(&self) -> Self {
+        match self {
+            MessageChannelTx::Server { consensus, requests, other } => {
+                MessageChannelTx::Server {
+                    other: other.clone(),
+                    #[cfg(not(feature = "channel_custom_dump"))]
+                    requests: Arc::clone(requests),
+                    #[cfg(feature = "channel_custom_dump")]
+                    requests: requests.clone(),
+                    consensus: consensus.clone(),
+                }
+            }
+            MessageChannelTx::Client { replies, other } => {
+                MessageChannelTx::Client {
+                    other: other.clone(),
+                    replies: replies.clone(),
+                }
+            }
+        }
+    }
+}
+
+impl<S, O, P> MessageChannelTx<S, O, P> {
+    pub async fn send(&mut self, message: Message<S, O, P>) -> Result<()> {
+        match self {
+            MessageChannelTx::Server {
+                consensus,
+                requests, other
+            } => {
+                match message {
+                    Message::System(header, message) => {
+                        match message {
+                            SystemMessage::Request(message) => {
+                                let m = StoredMessage::new(header, message);
+
+                                #[cfg(not(feature = "channel_custom_dump"))]
+                                    {
+                                        'spin_lock: loop {
+                                            if let Some(mut current_batch) = requests.batch.try_lock() {
+                                                current_batch.push_back(m);
+                                                break 'spin_lock;
+                                            }
+                                            rt::yield_now().await;
+                                        }
+
+                                        requests.event.notify(1);
+                                    }
+
+                                #[cfg(feature = "channel_custom_dump")]
+                                    {
+                                        requests.send(m);
+                                    }
+
+                                Ok(())
+                            }
+                            SystemMessage::Consensus(message) => {
+                                let m = StoredMessage::new(header, message);
+                                consensus.send(m).await
+                            }
+                            message @ SystemMessage::Cst(_) => {
+                                other.send(Message::System(header, message)).await
+                            }
+                            message @ SystemMessage::ViewChange(_) => {
+                                other.send(Message::System(header, message)).await
+                            }
+                            message @ SystemMessage::ForwardedRequests(_) => {
+                                other.send(Message::System(header, message)).await
+                            }
+                            // drop other msgs
+                            _ => Ok(()),
+                        }
+                    }
+                    _ => other.send(message).await,
+                }
+            }
+            MessageChannelTx::Client { replies, other } => {
+                match message {
+                    Message::System(header, message) => {
+                        match message {
+                            SystemMessage::Reply(message) => {
+                                let m = StoredMessage::new(header, message);
+                                replies.send(m).await
+                            }
+                            // drop other msgs
+                            _ => Ok(()),
+                        }
+                    }
+                    _ => other.send(message).await,
+                }
+            }
+        }
+    }
+}
+
+impl<S, O, P> MessageChannelRx<S, O, P> {
+    pub async fn recv(&mut self) -> Result<Message<S, O, P>> {
+        match self {
+            MessageChannelRx::Server { consensus, requests, other } => {
+                #[cfg(not(feature = "channel_custom_dump"))]
+                    {
+                        let message = select! {
+                    result = consensus.recv() => {
+                        let (h, c) = result?.into_inner();
+                        Message::System(h, SystemMessage::Consensus(c))
+                    },
+                        //Handle reception of requests through the batcher
+                    result = requests.recv() => {
+                        let batch = result?;
+                        Message::RequestBatch(Utc::now(), batch)
+                    }
+                    ,
+                    result = other.recv() => {
+                        let message = result?;
+                        message
+                    },
+                };
+                        Ok(message)
+                    }
+                #[cfg(feature = "channel_custom_dump")]
+                    {
+                        let message = select! {
+                            result = consensus.recv() => {
+                                let (h, c) = result?.into_inner();
+                                Message::System(h, SystemMessage::Consensus(c))
+                            },
+                        //Handle reception of requests through the batcher
+                            result = requests.recv_mult_async() => {
+                                let batch = result?;
+                                Message::RequestBatch(Utc::now(), batch)
+                            },
+                            result = other.recv() => {
+                                let message = result?;
+                                message
+                            },
+                };
+
+                        Ok(message)
+                    }
+            }
+            MessageChannelRx::Client { replies, other } => {
+                let message = select! {
+                    result = replies.recv() => {
+                        let (h, r) = result?.into_inner();
+                        Message::System(h, SystemMessage::Reply(r))
+                    },
+                    result = other.recv() => {
+                        let message = result?;
+                        message
+                    },
+                };
+                Ok(message)
+            }
+        }
+    }
 }
 
 pub struct RequestBatcherShared<O> {
@@ -167,6 +403,7 @@ pub struct RequestBatcher<O> {
 impl<O: Send + 'static> RequestBatcher<O> {
     pub fn spawn(mut self, max_batch_size: usize) {
         // handle events to prepare new batch
+
         rt::spawn(async move {
             loop {
                 let batch = 'new_batch: loop {
@@ -206,164 +443,5 @@ impl<O: Send + 'static> RequestBatcher<O> {
                     .await;
             }
         });
-    }
-}
-
-/// Creates a new channel that can queue up to `bound` messages
-/// from different async senders.
-pub fn new_message_channel<S, O, P>(
-    client: bool,
-    bound: usize,
-) -> (MessageChannelTx<S, O, P>, MessageChannelRx<S, O, P>, Option<RequestBatcher<O>>) {
-    if client {
-        let (rr_tx, rr_rx) = new_bounded(bound);
-        let (o_tx, o_rx) = new_bounded(bound);
-
-        let tx = MessageChannelTx::Client { replies: rr_tx, other: o_tx };
-        let rx = MessageChannelRx::Client { replies: rr_rx, other: o_rx };
-
-        (tx, rx, None)
-    } else {
-        let (c_tx, c_rx) = new_bounded(bound);
-        let (o_tx, o_rx) = new_bounded(bound);
-
-        let (reqbatch_tx, reqbatch_rx) = new_bounded(bound);
-        let shared = Arc::new(RequestBatcherShared {
-            event: Event::new(),
-            batch: Mutex::new(LinkedList::new()),
-        });
-
-        let batcher = Some(RequestBatcher {
-            requests: Arc::clone(&shared),
-            to_core_server_task: reqbatch_tx,
-        });
-        let tx = MessageChannelTx::Server {
-            consensus: c_tx,
-            requests: shared,
-            other: o_tx,
-        };
-        let rx = MessageChannelRx::Server {
-            consensus: c_rx,
-            requests: reqbatch_rx,
-            other: o_rx,
-        };
-        (tx, rx, batcher)
-    }
-}
-
-impl<S, O, P> Clone for MessageChannelTx<S, O, P> {
-    fn clone(&self) -> Self {
-        match self {
-            MessageChannelTx::Server { consensus, requests, other } => {
-                MessageChannelTx::Server {
-                    requests: Arc::clone(requests),
-                    consensus: consensus.clone(),
-                    other: other.clone(),
-                }
-            },
-            MessageChannelTx::Client { replies, other } => {
-                MessageChannelTx::Client {
-                    other: other.clone(),
-                    replies: replies.clone(),
-                }
-            },
-        }
-    }
-}
-
-impl<S, O, P> MessageChannelTx<S, O, P> {
-    pub async fn send(&mut self, message: Message<S, O, P>) -> Result<()> {
-        match self {
-            MessageChannelTx::Server { consensus, requests, other } => {
-                match message {
-                    Message::System(header, message) => {
-                        match message {
-                            SystemMessage::Request(message) => {
-                                let m = StoredMessage::new(header, message);
-
-                                'spin_lock: loop {
-                                    if let Some(mut current_batch) = requests.batch.try_lock() {
-                                        current_batch.push_back(m);
-                                        break 'spin_lock;
-                                    }
-                                    rt::yield_now().await;
-                                }
-
-                                requests.event.notify(1);
-                                Ok(())
-                            },
-                            SystemMessage::Consensus(message) => {
-                                let m = StoredMessage::new(header, message);
-                                consensus.send(m).await
-                            },
-                            message @ SystemMessage::Cst(_) => {
-                                other.send(Message::System(header, message)).await
-                            },
-                            message @ SystemMessage::ViewChange(_) => {
-                                other.send(Message::System(header, message)).await
-                            },
-                            message @ SystemMessage::ForwardedRequests(_) => {
-                                other.send(Message::System(header, message)).await
-                            },
-                            // drop other msgs
-                            _ => Ok(()),
-                        }
-                    },
-                    _ => other.send(message).await,
-                }
-            },
-            MessageChannelTx::Client { replies, other } => {
-                match message {
-                    Message::System(header, message) => {
-                        match message {
-                            SystemMessage::Reply(message) => {
-                                let m = StoredMessage::new(header, message);
-                                replies.send(m).await
-                            },
-                            // drop other msgs
-                            _ => Ok(()),
-                        }
-                    },
-                    _ => other.send(message).await,
-                }
-            },
-        }
-    }
-}
-
-impl<S, O, P> MessageChannelRx<S, O, P> {
-    pub async fn recv(&mut self) -> Result<Message<S, O, P>> {
-        match self {
-            MessageChannelRx::Server { consensus, requests, other } => {
-                let message = select! {
-                    result = consensus.recv() => {
-                        let (h, c) = result?.into_inner();
-                        Message::System(h, SystemMessage::Consensus(c))
-                    },
-                    result = requests.recv() => {
-                        let batch = result?;
-                        Message::RequestBatch(Utc::now(), batch)
-                    },
-                    result = other.recv() => {
-                        let message = result?;
-                        message
-                    },
-                };
-                Ok(message)
-            },
-            MessageChannelRx::Client { replies, other } => {
-                let message = select! {
-                    result = replies.recv() => {
-                        let (h, r) = result?.into_inner();
-                        Message::System(h, SystemMessage::Reply(r))
-                    },
-                    result = other.recv() => {
-                        let message = result?;
-                        message
-                    },
-                };
-                Ok(message)
-            },
-        }
     }
 }
