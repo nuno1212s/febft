25cae1388231eeb466ad845ebd4b1b9b6009cad2
Nuno Neto <nuno.neto.g@gmail.com>
2022-09-21 17:39:08 +0100

    Maybe fix logging (again)?

--- src/bft/mod.rs
+++ src/bft/mod.rs
@@ -73,7 +73,7 @@ pub unsafe fn init(c: InitConfig) -> Result<Option<InitGuard>> {
 
     //env_logger::init();
     let appender = FileAppender::builder()
-        .encoder(Box::new(PatternEncoder::new("{d} - {m}{n} {l} ")))
+        .encoder(Box::new(PatternEncoder::new("{l} {d} - {m}{n}")))
         .build(path)
         .wrapped(ErrorKind::ConsensusLog)?;
 
